d0430ffa2b80b81d13e58e4b9f475fa6a198c014
Carson <carsonpcase@gmail.com>
2024-07-01 12:12:56 -0400

    Created OAppAuth to be compatible with Auth contracts

--- src/base/Roles/CrossChain/CrossChainLayerZeroTellerWithMultiAssetSupport.sol
+++ src/base/Roles/CrossChain/CrossChainLayerZeroTellerWithMultiAssetSupport.sol
@@ -2,13 +2,16 @@
 pragma solidity 0.8.21;
 
 import {CrossChainTellerBase, BridgeData, ERC20} from "./CrossChainTellerBase.sol";
-
+import {OAppAuth, MessagingFee, Origin } from "./OAppAuth/OAppAuth.sol";
 import {console} from "@forge-std/Test.sol";
+import {Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {Auth} from "@solmate/auth/Auth.sol";
 
-contract CrossChainLayerZeroTellerWithMultiAssetSupport is CrossChainTellerBase{
+contract CrossChainLayerZeroTellerWithMultiAssetSupport is CrossChainTellerBase, OAppAuth{
     
-    constructor(address _owner, address _vault, address _accountant, address _weth)
+    constructor(address _owner, address _vault, address _accountant, address _weth, address _endpoint)
         CrossChainTellerBase(_owner, _vault, _accountant, _weth)
+        OAppAuth(_endpoint, _owner) 
     {
 
     }
---
+++ src/base/Roles/CrossChain/OAppAuth/OAppAuth.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+// @dev Import the 'MessagingFee' and 'MessagingReceipt' so it's exposed to OApp implementers
+// solhint-disable-next-line no-unused-import
+import { MessagingFee, MessagingReceipt } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OAppSender.sol";
+// @dev Import the 'Origin' so it's exposed to OApp implementers
+// solhint-disable-next-line no-unused-import
+import {Origin} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OAppReceiver.sol";
+
+import {OAppAuthCore} from "./OAppAuthCore.sol";
+import {OAppAuthReceiver} from "./OAppAuthReceiver.sol";
+import {OAppAuthSender} from "./OAppAuthSender.sol";
+
+/**
+ * @title OAppAuth
+ * @dev Abstract contract serving as the base for OApp implementation, combining OAppSender and OAppReceiver functionality.
+ * 
+ * @dev This Auth version of OAppCore uses solmate's Auth instead of OZ's Ownable for compatability purposes
+ */
+abstract contract OAppAuth is OAppAuthSender, OAppAuthReceiver {
+    /**
+     * @dev Constructor to initialize the OApp with the provided endpoint and owner.
+     * @param _endpoint The address of the LOCAL LayerZero endpoint.
+     * @param _delegate The delegate capable of making OApp configurations inside of the endpoint.
+     */
+    constructor(address _endpoint, address _delegate) OAppAuthCore(_endpoint, _delegate) {}
+
+    /**
+     * @notice Retrieves the OApp version information.
+     * @return senderVersion The version of the OAppSender.sol implementation.
+     * @return receiverVersion The version of the OAppReceiver.sol implementation.
+     */
+    function oAppVersion()
+        public
+        pure
+        virtual
+        override(OAppAuthSender, OAppAuthReceiver)
+        returns (uint64 senderVersion, uint64 receiverVersion)
+    {
+        return (SENDER_VERSION, RECEIVER_VERSION);
+    }
+}
