115063898e7611151f5a92a295cf4e25acdd6094
Jun Kim <64379343+junkim012@users.noreply.github.com>
2024-08-22 18:37:54 +0900

    feat: deployment script for generic rate provider

--- script/deploy/DeployGenericRateProvider.s.sol
+++ script/deploy/DeployGenericRateProvider.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.21;
 import { GenericRateProvider } from "./../../src/helper/GenericRateProvider.sol";
 import { BaseScript } from "../Base.s.sol";
 import { stdJson as StdJson } from "@forge-std/StdJson.sol";
+import { console2 } from "forge-std/console2.sol";
 
 using StdJson for string;
 
@@ -12,20 +13,27 @@ contract DeployGenericRateProvider is BaseScript {
     string configPath = "./deployment-config/rates/DeployGenericRateProvider.json";
     string config = vm.readFile(configPath);
 
-    uint256 expectedMin = config.readUint("expectedMin");
-    uint256 expectedMax = config.readUint("expectedMax");
-    address target = config.readAddress("target");
-    string signature = config.readString("signature");
-    bytes32 salt = config.readBytes32("salt");
+    uint256 expectedMin = config.readUint(".expectedMin");
+    uint256 expectedMax = config.readUint(".expectedMax");
+    address target = config.readAddress(".target");
+    string signature = config.readString(".signature");
+    bytes32 salt = config.readBytes32(".salt");
 
-    function run() public returns (address rateProvider) {
+    function run() public returns (GenericRateProvider rateProvider) {
         bytes4 functionSig = bytes4(keccak256(bytes(signature)));
+        console2.logBytes4(functionSig);
 
         bytes memory creationCode = type(GenericRateProvider).creationCode;
-        GenericRateProvider rateProvider =
-            GenericRateProvider(CREATEX.deployCreate3(salt, abi.encodePacked(functionSig)));
+
+        rateProvider = GenericRateProvider(
+            CREATEX.deployCreate3(
+                salt, abi.encodePacked(creationCode, abi.encode(target, functionSig, 0, 0, 0, 0, 0, 0, 0, 0))
+            )
+        );
 
         uint256 rate = rateProvider.getRate();
+
+        console2.log("rate: ", rate);
 
         require(rate != 0, "rate must not be zero");
         require(rate >= expectedMin, "rate must be greater than or equal to min");
