7cf84b05c825836bbaed014cc05b48d3a0d760ea
Carson <carsonpcase@gmail.com>
2024-08-26 13:03:31 -0400

    test: passing test for rate change

--- test/LiveDeploy.t.sol
+++ test/LiveDeploy.t.sol
@@ -12,10 +12,13 @@ import { TellerWithMultiAssetSupport } from "src/base/Roles/TellerWithMultiAsset
 import { AccountantWithRateProviders } from "src/base/Roles/AccountantWithRateProviders.sol";
 import { RolesAuthority } from "@solmate/auth/authorities/RolesAuthority.sol";
 
+string constant RPC_URL_ENV = "MAINNET_RPC_URL";
+string constant FILE_NAME = "exampleL1.json";
+
 // We use this so that we can use the inheritance linearization to start the fork before other constructors
 abstract contract ForkTest is Test {
     constructor() {
-        _startFork("MAINNET_RPC_URL");
+        _startFork(RPC_URL_ENV);
     }
 
     function _startFork(string memory rpcKey) internal virtual returns (uint256 forkId) {
@@ -31,20 +34,67 @@ contract LiveDeploy is ForkTest, DeployAll {
     uint8 constant SOLVER_ROLE = 42;
 
     function setUp() public virtual {
-        // we have to start the fork again... I don't exactly know why
-        _startFork("MAINNET_RPC_URL");
+        // we have to start the fork again... I don't exactly know why. But it's a known issue with foundry re:
+        // https://github.com/foundry-rs/foundry/issues/5471
+        _startFork(RPC_URL_ENV);
 
-        // Setup forked environment.
-        run("exampleL1.json");
+        // Run the deployment scripts
+        run(FILE_NAME);
+        // warp forward the minimumUpdateDelay for the accountant to prevent it from pausing on update test
+        vm.warp(block.timestamp + mainConfig.minimumUpdateDelayInSeconds);
 
+        // define one share based off of vault decimals
         ONE_SHARE = 10 ** BoringVault(payable(mainConfig.boringVault)).decimals();
 
         // give this the SOLVER_ROLE to call bulkWithdraw
         RolesAuthority rolesAuthority = RolesAuthority(mainConfig.rolesAuthority);
         vm.startPrank(mainConfig.protocolAdmin);
         rolesAuthority.setUserRole(address(this), SOLVER_ROLE, true);
-        rolesAuthority.setRoleCapability(SOLVER_ROLE, mainConfig.teller, TellerWithMultiAssetSupport.bulkWithdraw.selector, true);
+        rolesAuthority.setRoleCapability(
+            SOLVER_ROLE, mainConfig.teller, TellerWithMultiAssetSupport.bulkWithdraw.selector, true
+        );
         vm.stopPrank();
+    }
+
+    function testDepositBaseAssetAndUpdateRate(uint256 depositAmount, uint96 rateChange) public {
+        AccountantWithRateProviders accountant = AccountantWithRateProviders(mainConfig.accountant);
+        // manual bounding done because bound() doesn't exist for uint96
+        rateChange = rateChange % uint96(mainConfig.allowedExchangeRateChangeUpper - 1);
+        rateChange = (rateChange < mainConfig.allowedExchangeRateChangeLower + 1)
+            ? mainConfig.allowedExchangeRateChangeLower + 1
+            : rateChange;
+
+        // mint a bunch of extra tokens to the vault for if rate increased
+        deal(mainConfig.base, mainConfig.boringVault, depositAmount);
+
+        depositAmount = bound(depositAmount, 1, 10_000e18);
+
+        _depositAssetWithApprove(ERC20(mainConfig.base), depositAmount);
+        BoringVault boringVault = BoringVault(payable(mainConfig.boringVault));
+        uint256 expected_shares = depositAmount;
+        assertEq(
+            boringVault.balanceOf(address(this)),
+            expected_shares,
+            "Should have received expected shares 1:1 for base asset"
+        );
+
+        // update the rate
+        vm.startPrank(mainConfig.exchangeRateBot);
+        uint96 newRate = uint96(accountant.getRate()) * rateChange / 10_000;
+        accountant.updateExchangeRate(newRate);
+        vm.stopPrank();
+
+        uint256 expectedAssetsBack = depositAmount * rateChange / 10_000;
+
+        // attempt a withdrawal after
+        TellerWithMultiAssetSupport(mainConfig.teller).bulkWithdraw(
+            ERC20(mainConfig.base), expected_shares, expectedAssetsBack, address(this)
+        );
+        assertEq(
+            ERC20(mainConfig.base).balanceOf(address(this)),
+            expectedAssetsBack,
+            "Should have been able to withdraw back the depositAmount with rate factored"
+        );
     }
 
     function testDepositBaseAsset(uint256 depositAmount) public {
@@ -60,8 +110,14 @@ contract LiveDeploy is ForkTest, DeployAll {
         );
 
         // attempt a withdrawal after
-        TellerWithMultiAssetSupport(mainConfig.teller).bulkWithdraw(ERC20(mainConfig.base), expected_shares, depositAmount, address(this));
-        assertEq(ERC20(mainConfig.base).balanceOf(address(this)), depositAmount, "Should have been able to withdraw back the depositAmount");
+        TellerWithMultiAssetSupport(mainConfig.teller).bulkWithdraw(
+            ERC20(mainConfig.base), expected_shares, depositAmount, address(this)
+        );
+        assertEq(
+            ERC20(mainConfig.base).balanceOf(address(this)),
+            depositAmount,
+            "Should have been able to withdraw back the depositAmount"
+        );
     }
 
     function testDepositASupportedAsset(uint256 depositAmount, uint256 indexOfSupported) public {
@@ -71,12 +127,12 @@ contract LiveDeploy is ForkTest, DeployAll {
 
         uint256 expecteShares;
         AccountantWithRateProviders accountant = AccountantWithRateProviders(mainConfig.accountant);
-        uint[] memory expectedSharesByAsset = new uint[](assetsCount);
+        uint256[] memory expectedSharesByAsset = new uint256[](assetsCount);
         for (uint256 i; i < assetsCount; ++i) {
-            expectedSharesByAsset[i] = 
+            expectedSharesByAsset[i] =
                 depositAmount.mulDivDown(ONE_SHARE, accountant.getRateInQuoteSafe(ERC20(mainConfig.assets[i])));
             expecteShares += expectedSharesByAsset[i];
-            
+
             _depositAssetWithApprove(ERC20(mainConfig.assets[i]), depositAmount);
         }
 
@@ -84,11 +140,17 @@ contract LiveDeploy is ForkTest, DeployAll {
         assertEq(boringVault.balanceOf(address(this)), expecteShares, "Should have received expected shares");
 
         // withdrawal the assets for the same amount back
-        for(uint256 i; i < assetsCount; ++i){
-            TellerWithMultiAssetSupport(mainConfig.teller).bulkWithdraw(ERC20(mainConfig.assets[i]), expectedSharesByAsset[i], depositAmount-1, address(this));
-            assertApproxEqAbs(ERC20(mainConfig.assets[i]).balanceOf(address(this)), depositAmount, 1, "Should have been able to withdraw back the depositAmounts");
+        for (uint256 i; i < assetsCount; ++i) {
+            TellerWithMultiAssetSupport(mainConfig.teller).bulkWithdraw(
+                ERC20(mainConfig.assets[i]), expectedSharesByAsset[i], depositAmount - 1, address(this)
+            );
+            assertApproxEqAbs(
+                ERC20(mainConfig.assets[i]).balanceOf(address(this)),
+                depositAmount,
+                1,
+                "Should have been able to withdraw back the depositAmounts"
+            );
         }
-
     }
 
     function _depositAssetWithApprove(ERC20 asset, uint256 depositAmount) internal {
