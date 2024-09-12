249aab55d00f7b7e6132f945f73151bb6b892ba1
Jun Kim <64379343+junkim012@users.noreply.github.com>
2024-09-11 20:55:24 -0400

    feat: the rate provider deployment script deploys either generic, chainlink, or redstone

--- script/deploy/01_DeployRateProviders.s.sol
+++ script/deploy/01_DeployRateProviders.s.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.21;
 
-import { IRateProvider } from "./../../../src/interfaces/IRateProvider.sol";
-import { EthPerWstEthRateProvider } from "./../../../src/oracles/EthPerWstEthRateProvider.sol";
-
+import { GenericRateProvider } from "./../../../src/helper/GenericRateProvider.sol";
 import { BaseScript } from "./../Base.s.sol";
 import { stdJson as StdJson } from "@forge-std/StdJson.sol";
 import { ConfigReader } from "../ConfigReader.s.sol";
-import { console } from "@forge-std/Test.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { EthPerTokenRateProvider, IPriceFeed } from "src/oracles/EthPerTokenRateProvider.sol";
+import { console2 } from "forge-std/console2.sol";
 
 /// NOTE This script must change based on the supported assets of each vault deployment.
 contract DeployRateProviders is BaseScript {
@@ -27,60 +25,141 @@ contract DeployRateProviders is BaseScript {
         _run(Strings.toString(block.chainid), config);
     }
 
+    /**
+     * For the assets specified in the teller, look at <chainId>.json, and if
+     * the rate provider does not exist, either deploy Chainlink, Redstone, or
+     * the Generic Rate Provider.
+     */
     function _run(string memory fileName, string memory config) internal {
         string memory chainConfig = getChainConfigFile();
 
         address[] memory assets = config.readAddressArray(".teller.assets");
-        uint256 maxTimeFromLastUpdate = config.readUint(".rateProvider.maxTimeFromLastUpdate");
+        uint256 maxTimeFromLastUpdate = chainConfig.readUint(".assetToRateProviderAndPriceFeed.maxTimeFromLastUpdate");
 
         for (uint256 i; i < assets.length; ++i) {
             require(assets[i].code.length > 0, "asset must have code");
             string memory rateProviderKey =
                 string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".rateProvider"));
+
+            string memory token = assets[i].toHexString();
+            console2.log("token: ", token);
+
             address rateProvider = chainConfig.readAddress(rateProviderKey);
+
             // must deploy new rate provider and set the value
             if (rateProvider == address(0)) {
-                address priceFeed = chainConfig.readAddress(
-                    string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".priceFeed"))
-                );
-                uint8 decimals = uint8(
-                    chainConfig.readUint(
-                        string(
-                            abi.encodePacked(".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".decimals")
-                        )
-                    )
-                );
-                string memory description = chainConfig.readString(
-                    string(
-                        abi.encodePacked(".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".description")
-                    )
-                );
+                address deployedAddress;
+
                 uint256 priceFeedType = chainConfig.readUint(
-                    string(
-                        abi.encodePacked(".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".priceFeedType")
-                    )
+                    string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", token, ".priceFeedType"))
                 );
-                rateProvider =
-                    deployRateProvider(description, priceFeed, maxTimeFromLastUpdate, decimals, priceFeedType);
+
+                // Generic Rate Provider
+                if (priceFeedType == 2) {
+                    address target = chainConfig.readAddress(
+                        string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", token, ".target"))
+                    );
+
+                    string memory signature = chainConfig.readString(
+                        string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", token, ".signature"))
+                    );
+
+                    uint256 arg = chainConfig.readUint(
+                        string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", token, ".arg"))
+                    );
+
+                    uint256 expectedMin = chainConfig.readUint(
+                        string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", token, ".expectedMin"))
+                    );
+
+                    uint256 expectedMax = chainConfig.readUint(
+                        string(abi.encodePacked(".assetToRateProviderAndPriceFeed.", token, ".expectedMax"))
+                    );
+
+                    deployedAddress = _deployGenericRateProvider(target, signature, arg, expectedMin, expectedMax);
+                } else {
+                    // Chainlink or Redstone
+                    address priceFeed = chainConfig.readAddress(
+                        string(
+                            abi.encodePacked(".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".priceFeed")
+                        )
+                    );
+                    uint8 decimals = uint8(
+                        chainConfig.readUint(
+                            string(
+                                abi.encodePacked(
+                                    ".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".decimals"
+                                )
+                            )
+                        )
+                    );
+                    string memory description = chainConfig.readString(
+                        string(
+                            abi.encodePacked(
+                                ".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".description"
+                            )
+                        )
+                    );
+                    uint256 priceFeedType = chainConfig.readUint(
+                        string(
+                            abi.encodePacked(
+                                ".assetToRateProviderAndPriceFeed.", assets[i].toHexString(), ".priceFeedType"
+                            )
+                        )
+                    );
+                    deployedAddress =
+                        _deployRateProvider(description, priceFeed, maxTimeFromLastUpdate, decimals, priceFeedType);
+                }
                 string memory chainConfigFilePath = string.concat(CONFIG_CHAIN_ROOT, fileName, ".json");
-                rateProvider.toHexString().write(chainConfigFilePath, rateProviderKey);
+                deployedAddress.toHexString().write(chainConfigFilePath, rateProviderKey);
+                console2.log("deployedAddress: ", deployedAddress);
             }
         }
     }
 
     function deploy(ConfigReader.Config memory config) public override broadcast returns (address) { }
 
-    function deployRateProvider(
+    function _deployGenericRateProvider(
+        address target,
+        string memory signature,
+        uint256 arg,
+        uint256 expectedMin,
+        uint256 expectedMax
+    )
+        internal
+        broadcast
+        returns (address)
+    {
+        bytes4 functionSig = bytes4(keccak256(bytes(signature)));
+
+        bytes memory creationCode = type(GenericRateProvider).creationCode;
+
+        GenericRateProvider rateProvider =
+            new GenericRateProvider(target, functionSig, bytes32(arg), 0, 0, 0, 0, 0, 0, 0);
+
+        uint256 rate = rateProvider.getRate();
+
+        console2.log("rate: ", rate);
+
+        require(rate != 0, "rate must not be zero");
+        require(rate >= expectedMin, "rate must be greater than or equal to min");
+        require(rate <= expectedMax, "rate must be less than or equal to max");
+
+        return address(rateProvider);
+    }
+
+    function _deployRateProvider(
         string memory description,
         address priceFeed,
         uint256 maxTimeFromLastUpdate,
         uint8 decimals,
         uint256 priceFeedType
     )
-        public
+        internal
         broadcast
         returns (address)
     {
+        console2.log("priceFeed: ", priceFeed);
         require(maxTimeFromLastUpdate > 0, "max time from last update = 0");
         require(priceFeed.code.length > 0, "price feed must have code");
 
