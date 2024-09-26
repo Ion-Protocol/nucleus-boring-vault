3c9689ac50571a869765d8e7799c4fbc71caa91b
Carson <carsonpcase@gmail.com>
2024-09-26 19:48:09 -0400

    refactor: formatting

--- src/base/DecodersAndSanitizers/LayerZeroOFTDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/LayerZeroOFTDecoderAndSanitizer.sol
@@ -11,16 +11,16 @@ contract LayerZeroOFTDecoderAndSanitizer is BaseDecoderAndSanitizer {
 
     constructor(address _boringVault) BaseDecoderAndSanitizer(_boringVault) { }
 
-    /*
-    _sendParam:
-    uint32 dstEid; // Destination endpoint ID.                                                              [VERIFY]
-    bytes32 to; // Recipient address.                                                                       [VERIFY]
+    /**
+    * @dev _sendParam:
+        uint32 dstEid; // Destination endpoint ID.                                                              [VERIFY]
+        bytes32 to; // Recipient address.                                                                       [VERIFY]
         uint256 amountLD; // Amount to send in local decimals.
         uint256 minAmountLD; // Minimum amount to send in local decimals.
         bytes extraOptions; // Additional options supplied by the caller to be used in the LayerZero message.
-        bytes composeMsg; // The composed message for the send() operation.                                 [NONE]
-        bytes oftCmd; // The OFT command to be executed, unused in default OFT implementations.             [NONE]
-    _fee:
+        bytes composeMsg; // The composed message for the send() operation.                                     [NONE]
+        bytes oftCmd; // The OFT command to be executed, unused in default OFT implementations.                 [NONE]
+    * @dev _fee:
         uint256 nativeFee;
         uint256 lzTokenFee;
     */
