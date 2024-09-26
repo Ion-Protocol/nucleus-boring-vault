a0fd5ec600089f4af41413ee5e3c58592bbbf173
Carson <carsonpcase@gmail.com>
2024-09-26 15:29:47 -0400

    refactor: comments

--- src/base/DecodersAndSanitizers/LayerZeroOFTDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/LayerZeroOFTDecoderAndSanitizer.sol
@@ -18,8 +18,8 @@ contract LayerZeroOFTDecoderAndSanitizer is BaseDecoderAndSanitizer {
         uint256 amountLD; // Amount to send in local decimals.
         uint256 minAmountLD; // Minimum amount to send in local decimals.
         bytes extraOptions; // Additional options supplied by the caller to be used in the LayerZero message.
-        bytes composeMsg; // The composed message for the send() operation.
-        bytes oftCmd; // The OFT command to be executed, unused in default OFT implementations.
+        bytes composeMsg; // The composed message for the send() operation.                                 [NONE]
+        bytes oftCmd; // The OFT command to be executed, unused in default OFT implementations.             [NONE]
     _fee:
         uint256 nativeFee;
         uint256 lzTokenFee;
