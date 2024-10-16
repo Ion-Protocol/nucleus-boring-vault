7f6540f40c37f55b648b8d45b61072af51e1a27b
Jun Kim <64379343+junkim012@users.noreply.github.com>
2024-10-15 20:47:19 -0400

    chore: pufETH generic rate provider

--- script/deploy/DeployGenericRateProvider.s.sol
+++ script/deploy/DeployGenericRateProvider.s.sol
@@ -8,7 +8,7 @@ import { console2 } from "forge-std/console2.sol";
 
 using StdJson for string;
 
-// NOTE Currently assumes that function signature arguments are empty.
+// NOTE Currently assumes that there is only one function signature argument.
 contract DeployGenericRateProvider is BaseScript {
     string configPath = "./deployment-config/rates/DeployGenericRateProvider.json";
     string config = vm.readFile(configPath);
@@ -16,24 +16,31 @@ contract DeployGenericRateProvider is BaseScript {
     uint256 expectedMin = config.readUint(".expectedMin");
     uint256 expectedMax = config.readUint(".expectedMax");
     address target = config.readAddress(".target");
+    uint256 arg1 = config.readUint(".arg1");
     string signature = config.readString(".signature");
     bytes32 salt = config.readBytes32(".salt");
 
     function run() public broadcast returns (GenericRateProvider rateProvider) {
         bytes4 functionSig = bytes4(keccak256(bytes(signature)));
+        
+        console2.log('constructor args');
+        console2.logBytes(abi.encode(target, functionSig, arg1, bytes32(0), bytes32(0), bytes32(0), bytes32(0), bytes32(0), bytes32(0), bytes32(0)));
+        
         console2.logBytes4(functionSig);
 
         bytes memory creationCode = type(GenericRateProvider).creationCode;
 
         rateProvider = GenericRateProvider(
             CREATEX.deployCreate3(
-                salt, abi.encodePacked(creationCode, abi.encode(target, functionSig, 0, 0, 0, 0, 0, 0, 0, 0))
+                salt, abi.encodePacked(creationCode, abi.encode(target, functionSig, arg1, 0, 0, 0, 0, 0, 0, 0))
             )
         );
 
         uint256 rate = rateProvider.getRate();
 
         console2.log("rate: ", rate);
+        console2.log("expectedMin: ", expectedMin);
+        console2.log("expectedMax: ", expectedMax);
 
         require(rate != 0, "rate must not be zero");
         require(rate >= expectedMin, "rate must be greater than or equal to min");
