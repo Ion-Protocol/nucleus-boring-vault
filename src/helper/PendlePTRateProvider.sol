caa663fa21a87ede48fc5a6ac82afdd91116e6b2
Carson <carsonpcase@gmail.com>
2024-12-20 10:15:45 +0700

    feat: decimal check on pendle rate provider

--- src/helper/PendlePTRateProvider.sol
+++ src/helper/PendlePTRateProvider.sol
@@ -8,6 +8,8 @@ import { IStandardizedYield } from
     "lib/ion-protocol/lib/pendle-core-v2-public/contracts/interfaces/IStandardizedYield.sol";
 import { IPPtLpOracle } from "lib/ion-protocol/lib/pendle-core-v2-public/contracts/interfaces/IPPtLpOracle.sol";
 
+error PendlePTRateProvider__InvalidDecimals(uint256 decimals);
+
 /**
  * @title PendlePTRateProvider
  * @custom:security-contact security@molecularlabs.io
@@ -27,10 +29,12 @@ contract PendlePTRateProvider is IRateProvider {
 
     /// @notice getRate for a Pendle PT token
     function getRate() external view returns (uint256) {
-        (IStandardizedYield sy,,) = market.readTokens();
+        (IStandardizedYield sy, IStrategy pt,) = market.readTokens();
         uint256 syRate = sy.exchangeRate();
         uint256 ptRate = ORACLE.getPtToAssetRate(address(market), DURATION);
-
+        if (sy.decimals() != 18 || py.decimals() != 18) {
+            revert PendlePTRateProvider__InvalidDecimals(sy.decimals());
+        }
         return syRate * ptRate / 1e18;
     }
 }
