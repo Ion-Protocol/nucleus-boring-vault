c58195d762741d2578999152bdd2b58144af152d
Carson <carsonpcase@gmail.com>
2025-02-07 15:12:49 -0500

    feat: puffer added curve + balancer

--- src/base/DecodersAndSanitizers/unifiUSDDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/unifiUSDDecoderAndSanitizer.sol
@@ -10,14 +10,18 @@ import { PendleRouterDecoderAndSanitizer } from
     "src/base/DecodersAndSanitizers/Protocols/PendleRouterDecoderAndSanitizer.sol";
 import { LevelDecoderAndSanitizer } from "./Protocols/LevelDecoderAndSanitizer.sol";
 import { ERC4626DecoderAndSanitizer } from "./Protocols/ERC4626DecoderAndSanitizer.sol";
+import { CurveDecoderAndSanitizer } from "./Protocols/CurveDecoderAndSanitizer.sol";
+import { BalancerV2DecoderAndSanitizer } from "./Protocols/BalancerV2DecoderAndSanitizer.sol";
 
-contract UnifiETHDecoderAndSanitizer is
+contract UnifiUSDDecoderAndSanitizer is
     NativeWrapperDecoderAndSanitizer,
     UniswapV3DecoderAndSanitizer,
     MasterChefV3DecoderAndSanitizer,
     PendleRouterDecoderAndSanitizer,
     LevelDecoderAndSanitizer,
-    ERC4626DecoderAndSanitizer
+    ERC4626DecoderAndSanitizer,
+    CurveDecoderAndSanitizer,
+    BalancerV2DecoderAndSanitizer
 {
     constructor(
         address _boringVault,
@@ -26,4 +30,34 @@ contract UnifiETHDecoderAndSanitizer is
         UniswapV3DecoderAndSanitizer(_uniswapV3NonFungiblePositionManager)
         BaseDecoderAndSanitizer(_boringVault)
     { }
+
+    /**
+     * @notice BalancerV2, NativeWrapper, Curve all specify a `withdraw(uint256)`,
+     *         all cases are handled the same way.
+     */
+    function withdraw(uint256)
+        external
+        pure
+        override(BalancerV2DecoderAndSanitizer, CurveDecoderAndSanitizer, NativeWrapperDecoderAndSanitizer)
+        returns (bytes memory addressesFound)
+    {
+        // Nothing to sanitize or return
+        return addressesFound;
+    }
+
+    /**
+     * @notice Curve, BalancerV2, and ERC4626 all specify a `deposit(uint256, address receiver)`,
+     *         all cases are handled the same way.
+     */
+    function deposit(
+        uint256,
+        address receiver
+    )
+        external
+        pure
+        override(CurveDecoderAndSanitizer, BalancerV2DecoderAndSanitizer, ERC4626DecoderAndSanitizer)
+        returns (bytes memory addressesFound)
+    {
+        addressesFound = abi.encodePacked(receiver);
+    }
 }
