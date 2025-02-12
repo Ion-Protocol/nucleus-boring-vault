8232a99e2b47bccf5b3088b02df10cf15688afb0
Carson <carsonpcase@gmail.com>
2025-02-12 18:42:19 -0500

    chore merged carson/ssETH-decoder

--- src/base/DecodersAndSanitizers/Protocols/PirexEthDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/PirexEthDecoderAndSanitizer.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.21;
 import { BaseDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
 
 abstract contract PirexEthDecoderAndSanitizer is BaseDecoderAndSanitizer {
-    function deposit(address receiver, bool) external returns (bytes memory) {
+    function deposit(address receiver, bool) external view returns (bytes memory) {
         if (receiver != boringVault) {
             revert NotVault();
         }
