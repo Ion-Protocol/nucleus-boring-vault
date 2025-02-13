57af02305123708e1004e8f5947c6d380c3caf49
Jun Kim <64379343+junkim012@users.noreply.github.com>
2025-02-12 19:00:24 -0500

    feat: looped hype depositor with external internal

--- src/helper/LoopedHypeDepositor.sol
+++ src/helper/LoopedHypeDepositor.sol
@@ -1,15 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { TellerWithMultiAssetSupport } from "../base/Roles/TellerWithMultiAssetSupport.sol";
-import { ERC20 } from "@solmate/tokens/ERC20.sol";
-import { Auth, Authority } from "solmate/auth/Auth.sol";
-import { SafeTransferLib } from "@solmate/utils/SafeTransferLib.sol";
+import {TellerWithMultiAssetSupport} from "../base/Roles/TellerWithMultiAssetSupport.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+import {Auth, Authority} from "solmate/auth/Auth.sol";
+import {SafeTransferLib} from "@solmate/utils/SafeTransferLib.sol";
+
+interface IWHYPE {
+    function balanceOf(address account) external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+    
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+
+}
 
 contract LoopedHypeDepositor is Auth {
     using SafeTransferLib for ERC20;
 
     error ZeroAddress();
+    error IncorrectNativeDepositAmount();
+
+    IWHYPE constant WHYPE = IWHYPE(0x5555555555555555555555555555555555555555);
 
     TellerWithMultiAssetSupport public immutable teller;
     address public immutable boringVault;
@@ -26,7 +40,10 @@ contract LoopedHypeDepositor is Auth {
         bytes communityCode
     );
 
-    constructor(address _teller, address _owner) Auth(_owner, Authority(address(0))) {
+    constructor(
+        address _teller,
+        address _owner
+    ) Auth(_owner, Authority(address(0))) {
         if (_teller == address(0)) revert ZeroAddress();
         if (_owner == address(0)) revert ZeroAddress();
 
@@ -37,6 +54,30 @@ contract LoopedHypeDepositor is Auth {
     }
 
     /**
+     * @notice For depositing the native asset of the chain
+     * @param depositAmount The amount to deposit
+     * @param minimumMint Minimum amount of shares to mint. Reverts otherwise
+     * @param to The recipient of the shares
+     * @param communityCode Indicator for which operator the token gets staked to
+     */
+    function depositNative(
+        uint256 depositAmount,
+        uint256 minimumMint,
+        address to,
+        bytes calldata communityCode
+    ) external payable requiresAuth returns (uint256 shares) {
+        if (msg.value != depositAmount) revert IncorrectNativeDepositAmount();
+        WHYPE.deposit{value: msg.value}();
+        return _deposit(
+            ERC20(address(WHYPE)),
+            depositAmount,
+            minimumMint,
+            to,
+            communityCode
+        );
+    }
+    
+    /**
      * @notice Deposits tokens and emits an event with a unique hash
      * @param depositAsset The ERC20 token to deposit
      * @param depositAmount The amount to deposit
@@ -44,26 +85,50 @@ contract LoopedHypeDepositor is Auth {
      * @param to The recipient of the shares
      * @param communityCode Indicator for which operator the token gets staked to
      */
-    function depositWithCommunityCode(
+    function deposit(
         ERC20 depositAsset,
         uint256 depositAmount,
         uint256 minimumMint,
         address to,
         bytes calldata communityCode
-    )
-        external
-        requiresAuth
-        returns (uint256 shares)
-    {
-        bytes32 depositHash = keccak256(abi.encodePacked(address(this), depositNonce++));
-
+    ) external requiresAuth returns (uint256 shares) {
         depositAsset.safeTransferFrom(msg.sender, address(this), depositAmount);
+        return
+            _deposit(
+                depositAsset,
+                depositAmount,
+                minimumMint,
+                to,
+                communityCode
+            );
+    }
+
+    /**
+     * Always assumes that the `depositAsset` is on this contract's balance.
+     */ 
+    function _deposit(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint,
+        address to,
+        bytes calldata communityCode
+    ) internal returns (uint256 shares) {
+        bytes32 depositHash = keccak256(
+            abi.encodePacked(address(this), depositNonce++)
+        );
+
         depositAsset.safeApprove(boringVault, depositAmount);
 
         shares = teller.deposit(depositAsset, depositAmount, minimumMint, to);
 
         emit DepositWithCommunityCode(
-            msg.sender, depositAsset, depositAmount, minimumMint, to, depositHash, communityCode
+            msg.sender,
+            depositAsset,
+            depositAmount,
+            minimumMint,
+            to,
+            depositHash,
+            communityCode
         );
     }
 }
