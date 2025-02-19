8d5081e5bc328265645aaf23870d2b4c3f37b386
Carson <carsonpcase@gmail.com>
2025-02-19 00:26:14 -0500

    feat: Hyperliquid decoder

---
+++ src/base/DecodersAndSanitizers/HyperliquidDecoderAndSanitizer.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.21;
+
+import {
+    PendleRouterDecoderAndSanitizer,
+    BaseDecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/PendleRouterDecoderAndSanitizer.sol";
+import { UniswapV3DecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/UniswapV3DecoderAndSanitizer.sol";
+import { OneInchDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/OneInchDecoderAndSanitizer.sol";
+import { CurveDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/CurveDecoderAndSanitizer.sol";
+import { NativeWrapperDecoderAndSanitizer } from
+    "src/base/DecodersAndSanitizers/Protocols/NativeWrapperDecoderAndSanitizer.sol";
+import { ERC4626DecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/ERC4626DecoderAndSanitizer.sol";
+import { EigenpieDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/EigenpieDecoderAndSanitizer.sol";
+import { PirexEthDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/PirexEthDecoderAndSanitizer.sol";
+import { StakingRewardsDecoderAndSanitizer } from
+    "src/base/DecodersAndSanitizers/Protocols/StakingRewardsDecoderAndSanitizer.sol";
+
+contract HyperliquidDecoderAndSanitizer is
+    PendleRouterDecoderAndSanitizer,
+    UniswapV3DecoderAndSanitizer,
+    OneInchDecoderAndSanitizer,
+    CurveDecoderAndSanitizer,
+    NativeWrapperDecoderAndSanitizer,
+    ERC4626DecoderAndSanitizer,
+    EigenpieDecoderAndSanitizer,
+    PirexEthDecoderAndSanitizer,
+    StakingRewardsDecoderAndSanitizer
+{
+    constructor(
+        address _boringVault,
+        address _uniswapV3NonFungiblePositionManager
+    )
+        BaseDecoderAndSanitizer(_boringVault)
+        UniswapV3DecoderAndSanitizer(_uniswapV3NonFungiblePositionManager)
+    { }
+
+    function deposit(
+        uint256,
+        address receiver
+    )
+        external
+        pure
+        override(CurveDecoderAndSanitizer, ERC4626DecoderAndSanitizer)
+        returns (bytes memory addressesFound)
+    {
+        addressesFound = abi.encodePacked(receiver);
+    }
+
+    function withdraw(uint256)
+        external
+        pure
+        override(CurveDecoderAndSanitizer, NativeWrapperDecoderAndSanitizer)
+        returns (bytes memory addressesFound)
+    {
+        // Nothing to sanitize or return
+        return addressesFound;
+    }
+}
---
+++ src/base/DecodersAndSanitizers/Protocols/StakingRewardsDecoderAndSanitizer.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { BaseDecoderAndSanitizer } from "../BaseDecoderAndSanitizer.sol";
+
+abstract contract StakingRewardsDecoderAndSanitizer is BaseDecoderAndSanitizer {
+    error StakingRewardsDecoderAndSanitizer__InvalidReceiver();
+
+    function mint(address to, string calldata communityCode) external view returns (bytes memory addressesFound) {
+        if (to != boringVault) {
+            revert StakingRewardsDecoderAndSanitizer__InvalidReceiver();
+        }
+        return addressesFound;
+    }
+
+    function mint(address to) external view returns (bytes memory addressesFound) {
+        if (to != boringVault) {
+            revert StakingRewardsDecoderAndSanitizer__InvalidReceiver();
+        }
+        return addressesFound;
+    }
+
+    function burnAndRedeemIfPossible(
+        address to,
+        uint256 amount,
+        string calldata communityCode
+    )
+        external
+        view
+        returns (bytes memory addressesFound)
+    {
+        if (to != boringVault) {
+            revert StakingRewardsDecoderAndSanitizer__InvalidReceiver();
+        }
+        return addressesFound;
+    }
+
+    function burnAndRedeemIfPossible(address to, uint256 amount) external view returns (bytes memory addressesFound) {
+        if (to != boringVault) {
+            revert StakingRewardsDecoderAndSanitizer__InvalidReceiver();
+        }
+        return addressesFound;
+    }
+
+    function redeem(uint256 burnID) external pure returns (bytes memory addressesFound) {
+        return addressesFound;
+    }
+}
