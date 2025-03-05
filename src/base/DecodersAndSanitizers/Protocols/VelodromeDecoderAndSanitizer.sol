19a223a2e3b6e54208cf3005b75b20c60920e8cc
Carson <carsonpcase@gmail.com>
2025-03-05 13:29:55 -0500

    chore: committing work so far to merge into correct branch

---
+++ src/base/DecodersAndSanitizers/Protocols/VelodromeDecoderAndSanitizer.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { IVelodromeNonFungiblePositionManager } from "src/interfaces/IVelodromeNonFungiblePositionManager.sol";
+import { BaseDecoderAndSanitizer, DecoderCustomTypes } from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+abstract contract VelodromeDecoderAndSanitizer is BaseDecoderAndSanitizer {
+    //============================== ERRORS ===============================
+
+    error UniswapV3DecoderAndSanitizer__BadPathFormat();
+    error UniswapV3DecoderAndSanitizer__BadTokenId();
+
+    //============================== IMMUTABLES ===============================
+
+    /**
+     * @notice The networks velodrome nonfungible position manager.
+     */
+    IVelodromeNonFungiblePositionManager internal immutable velodromeNonFungiblePositionManager;
+
+    constructor(address _velodromeNonFungiblePositionManager) {
+        velodromeNonFungiblePositionManager = IVelodromeNonFungiblePositionManager(_velodromeNonFungiblePositionManager);
+    }
+
+    //============================== UNISWAP V3 ===============================
+
+    function mint(DecoderCustomTypes.VelodromeMintParams calldata params)
+        external
+        pure
+        virtual
+        returns (bytes memory addressesFound)
+    {
+        // Nothing to sanitize
+        // Return addresses found
+        addressesFound = abi.encodePacked(params.token0, params.token1, params.recipient);
+    }
+
+    function increaseLiquidity(DecoderCustomTypes.IncreaseLiquidityParams calldata params)
+        external
+        view
+        virtual
+        returns (bytes memory addressesFound)
+    {
+        // Sanitize raw data
+        if (uniswapV3NonFungiblePositionManager.ownerOf(params.tokenId) != boringVault) {
+            revert UniswapV3DecoderAndSanitizer__BadTokenId();
+        }
+        // Extract addresses from uniswapV3NonFungiblePositionManager.positions(params.tokenId).
+        (, address operator, address token0, address token1,,,,,,,,) =
+            uniswapV3NonFungiblePositionManager.positions(params.tokenId);
+        addressesFound = abi.encodePacked(operator, token0, token1);
+    }
+
+    function decreaseLiquidity(DecoderCustomTypes.DecreaseLiquidityParams calldata params)
+        external
+        view
+        virtual
+        returns (bytes memory addressesFound)
+    {
+        // Sanitize raw data
+        // NOTE ownerOf check is done in PositionManager contract as well, but it is added here
+        // just for completeness.
+        if (uniswapV3NonFungiblePositionManager.ownerOf(params.tokenId) != boringVault) {
+            revert UniswapV3DecoderAndSanitizer__BadTokenId();
+        }
+
+        // No addresses in data
+        return addressesFound;
+    }
+
+    function collect(DecoderCustomTypes.CollectParams calldata params)
+        external
+        view
+        virtual
+        returns (bytes memory addressesFound)
+    {
+        // Sanitize raw data
+        // NOTE ownerOf check is done in PositionManager contract as well, but it is added here
+        // just for completeness.
+        if (uniswapV3NonFungiblePositionManager.ownerOf(params.tokenId) != boringVault) {
+            revert UniswapV3DecoderAndSanitizer__BadTokenId();
+        }
+
+        // Return addresses found
+        addressesFound = abi.encodePacked(params.recipient);
+    }
+
+    function safeTransferFrom(
+        address,
+        address to,
+        uint256
+    )
+        external
+        pure
+        virtual
+        returns (bytes memory addressesFound)
+    {
+        addressesFound = abi.encodePacked(to);
+    }
+}
