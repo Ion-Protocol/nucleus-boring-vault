412168f161058d1f7a145dde93556c42797f86dd
Jamie Pickett <jpickett713@gmail.com>
2025-03-19 12:38:52 -0400

    chore: add back in some removed comments

--- src/base/Roles/ManagerWithMerkleVerification.sol
+++ src/base/Roles/ManagerWithMerkleVerification.sol
@@ -23,11 +23,29 @@ contract ManagerWithMerkleVerification is AuthOwnable2Step {
 
     // ========================================= STATE =========================================
 
-    // Mapping of strategist to their Merkle tree root.
+    /**
+     * @notice A merkle tree root that restricts what data can be passed to the BoringVault.
+     * @dev Maps a strategist address to their specific merkle root.
+     * @dev Each leaf is composed of the keccak256 hash of abi.encodePacked {decodersAndSanitizer, target,
+     * valueIsNonZero, selector, argumentAddress_0, ...., argumentAddress_N}
+     *      Where:
+     *             - decodersAndSanitizer is the address to call to extract packed address arguments from the calldata
+     *             - target is the address to make the call to
+     *             - valueIsNonZero is a bool indicating whether or not the value is non-zero
+     *             - selector is the function selector on target
+     *             - argumentAddress is each allowed address argument in that call
+     */
     mapping(address => bytes32) public manageRoot;
 
-    // Reentrancy / flash loan state.
+    /**
+     * @notice Bool indicating whether or not this contract is actively performing a flash loan.
+     * @dev Used to block flash loans that are initiated outside a manage call.
+     */
     bool internal performingFlashLoan;
+
+    /**
+     * @notice keccak256 hash of flash loan data.
+     */
     bytes32 internal flashLoanIntentHash = bytes32(0);
 
     // Temporary storage for the flash loan pool address during a flash loan callback.
@@ -35,7 +53,9 @@ contract ManagerWithMerkleVerification is AuthOwnable2Step {
     // For Morpho flash loans (only one asset at a time) we also store the token.
     address internal currentMorphoToken;
 
-    // Used to pause vault management.
+    /**
+     * @notice Used to pause calls to `manageVaultWithMerkleVerification`.
+     */
     bool public isPaused;
 
     // =============================== ERRORS ===============================
@@ -72,17 +92,29 @@ contract ManagerWithMerkleVerification is AuthOwnable2Step {
 
     // ========================================= ADMIN FUNCTIONS =========================================
 
+    /**
+     * @notice Sets the manageRoot.
+     * @dev Callable by OWNER_ROLE.
+     */
     function setManageRoot(address strategist, bytes32 _manageRoot) external requiresAuth {
         bytes32 oldRoot = manageRoot[strategist];
         manageRoot[strategist] = _manageRoot;
         emit ManageRootUpdated(strategist, oldRoot, _manageRoot);
     }
 
+    /**
+     * @notice Pause this contract, which prevents future calls to `manageVaultWithMerkleVerification`.
+     * @dev Callable by MULTISIG_ROLE.
+     */
     function pause() external requiresAuth {
         isPaused = true;
         emit Paused();
     }
 
+    /**
+     * @notice Unpause this contract, which allows future calls to `manageVaultWithMerkleVerification`.
+     * @dev Callable by MULTISIG_ROLE.
+     */
     function unpause() external requiresAuth {
         isPaused = false;
         emit Unpaused();
@@ -118,6 +150,13 @@ contract ManagerWithMerkleVerification is AuthOwnable2Step {
 
     // ========================================= STRATEGIST FUNCTIONS =========================================
 
+    /**
+     * @notice Allows strategist to manage the BoringVault.
+     * @dev The strategist must provide a merkle proof for every call that verifiees they are allowed to make that call.
+     * @dev Callable by MANAGER_INTERNAL_ROLE.
+     * @dev Callable by STRATEGIST_ROLE.
+     * @dev Callable by MICRO_MANAGER_ROLE.
+     */
     function manageVaultWithMerkleVerification(
         bytes32[][] calldata manageProofs,
         address[] calldata decodersAndSanitizers,
@@ -179,6 +218,9 @@ contract ManagerWithMerkleVerification is AuthOwnable2Step {
 
     /**
      * @notice Balancer flash loan callback.
+     * @dev userData can optionally have salt encoded at the end of it, in order to change the intentHash,
+     *      if a flash loan is exact userData is being repeated, and their is fear of 3rd parties
+     *      front-running the rebalance.
      * @param tokens The token addresses.
      * @param amounts The amounts borrowed.
      * @param feeAmounts (ignored; Balancer fees are assumed to be zero)
@@ -429,6 +471,7 @@ contract ManagerWithMerkleVerification is AuthOwnable2Step {
         internal
         view
     {
+        // Use address decoder to get addresses in call data.
         bytes memory packedArgumentAddresses = abi.decode(decoderAndSanitizer.functionStaticCall(targetData), (bytes));
         if (
             !_verifyManageProof(
