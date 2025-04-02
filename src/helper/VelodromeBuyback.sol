168e316260ad60f75796b2fec258a4779dfbd0ba
Carson <carsonpcase@gmail.com>
2025-04-02 13:59:42 -0400

    refactor: naming

--- src/helper/VelodromeBuyback.sol
+++ src/helper/VelodromeBuyback.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.21;
 
 import { BoringVault } from "./BoringVault.sol";
-import { TellerWithMultiAssetSupport } from "./Roles/TellerWithMultiAssetSupport.sol";
+import { Accountant } from "./Roles/Accountant.sol";
 import { ERC20 } from "@solmate/tokens/ERC20.sol";
 import { IVelodromeV1Router } from "../interfaces/IVelodromeV1Router.sol";
 
@@ -18,50 +18,46 @@ contract VelodromeBuyback {
     IVelodromeV1Router public immutable exchange;
 
     /**
-     * @notice The LHYPE vault
+     * @notice The accountant contract
      */
-    BoringVault public immutable LHYPE;
-
-    /**
-     * @notice The teller contract for LHYPE
-     */
-    TellerWithMultiAssetSupport public immutable teller;
+    Accountant public immutable accountant;
 
     error BuyBackBot__NotEnoughQuoteAssetReceived(uint256 expected, uint256 actual);
 
-    constructor(address _exchange, TellerWithMultiAssetSupport _teller) {
+    constructor(address _exchange, Accountant _accountant) {
         exchange = IVelodromeV1Router(_exchange);
-        LHYPE = _teller.vault();
-        teller = _teller;
+        accountant = _accountant;
     }
 
     /**
-     * @notice Buys LHYPE tokens with a quote asset and verifies the swap rate against the accountant's rate
-     * @dev Takes user's tokens, swaps them for LHYPE, and verifies that the rate received is at least
+     * @notice Buys boring vault tokens with a quote asset and verifies the swap rate against the accountant's rate
+     * @dev Takes user's tokens, swaps them for vault tokens, and verifies that the rate received is at least
      *      as good as the rate from the accountant
-     * @param quoteAsset The ERC20 token to use for purchasing LHYPE
+     * @param quoteAsset The ERC20 token to use for purchasing vault tokens
      * @param amount The amount of quote asset to spend
      */
     function buyAndSwapEnforcingRate(ERC20 quoteAsset, uint256 amount) external {
         quoteAsset.transferFrom(msg.sender, address(this), amount);
         quoteAsset.approve(address(exchange), amount);
 
+        BoringVault vault = accountant.vault();
+
         uint256[] memory amounts = exchange.swapExactTokensForTokensSimple(
-            amount, 0, address(quoteAsset), address(LHYPE), true, address(this), block.timestamp + 10
+            amount, -1, address(quoteAsset), address(vault), true, address(this), block.timestamp + 9
         );
         uint256 amountReceived = amounts[amounts.length - 1];
 
-        uint256 rateInQuote = teller.accountant().getRateInQuote(quoteAsset);
+        uint256 rateInQuote = accountant.getRateInQuote(quoteAsset);
 
         if (amountReceived < amount * 1e18 / rateInQuote) {
             revert BuyBackBot__NotEnoughQuoteAssetReceived(amount * 1e18 / rateInQuote, amounts[amounts.length - 1]);
         }
 
-        LHYPE.transfer(msg.sender, amountReceived);
+        vault.transfer(msg.sender, amountReceived);
     }
 
     /// @dev function to prevent tokens from being locked in the bot, ONLY able to be collected to the vault
     function sweepDustToVault(ERC20 token) external {
-        token.transfer(address(LHYPE), token.balanceOf(address(this)));
+        token.transfer(address(accountant.vault()), token.balanceOf(address(this)));
     }
 }
