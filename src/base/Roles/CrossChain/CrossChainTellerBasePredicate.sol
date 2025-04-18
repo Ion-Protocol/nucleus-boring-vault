8eda06c248f44c75b63671efd245e2dfc51e8c3f
Jamie Pickett <jpickett713@gmail.com>
2025-04-17 22:12:36 -0400

    feat: add predicate inherited patterns

---
+++ src/base/Roles/CrossChain/CrossChainTellerBasePredicate.sol
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { TellerWithMultiAssetSupport } from "../TellerWithMultiAssetSupportPredicate.sol";
+import { ERC20 } from "@solmate/tokens/ERC20.sol";
+import {PredicateClient} from "@predicate/contracts/src/mixins/PredicateClient.sol";
+import {PredicateMessage} from "@predicate/contracts/src/interfaces/IPredicateClient.sol";
+import {IPredicateManager} from "@predicate/contracts/src/interfaces/IPredicateManager.sol";
+
+struct BridgeData {
+    uint32 chainSelector;
+    address destinationChainReceiver;
+    ERC20 bridgeFeeToken;
+    uint64 messageGas;
+    bytes data;
+}
+
+/**
+ * @title CrossChainTellerBasePredicate
+ * @notice Base contract for the CrossChainTeller, includes functions to overload with specific bridge method
+ */
+abstract contract CrossChainTellerBasePredicate is TellerWithMultiAssetSupportPredicate {
+    event MessageSent(bytes32 messageId, uint256 shareAmount, address to);
+    event MessageReceived(bytes32 messageId, uint256 shareAmount, address to);
+
+    constructor(
+        address _owner,
+        address _vault,
+        address _accountant,
+        address _serviceManager,
+        string memory _policyID
+    )
+        TellerWithMultiAssetSupportPredicate(_owner, _vault, _accountant, _serviceManager, _policyID)
+    { }
+
+    /**
+     * @notice function to deposit into the vault AND bridge crosschain in 1 call
+     * @param depositAsset ERC20 to deposit
+     * @param depositAmount amount of deposit asset to deposit
+     * @param minimumMint minimum required shares to receive
+     * @param data Bridge Data
+     * @param predicateMessage Predicate message to authorize the transaction
+     */
+    function depositAndBridge(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint,
+        BridgeData calldata data,
+        PredicateMessage calldata predicateMessage
+    )
+        external
+        payable
+        requiresAuth
+        nonReentrant
+    {
+        bytes memory encodedSigAndArgs = abi.encodeWithSignature("_depositAndBridge(address,uint256,uint256,BridgeData)", depositAsset, depositAmount, minimumMint, data);
+        if(!_authorizeTransaction(predicateMessage, encodedSigAndArgs)){
+            revert TellerWithMultiAssetSupport__PredicateUnauthorizedTransaction();
+        }
+        _depositAndBridge(depositAsset, depositAmount, minimumMint, data);
+    }
+
+    /**
+     * @notice internal helper which conforms to Predicate Inheritance pattern     
+     */
+    function _depositAndBridge(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint,
+        BridgeData calldata data
+    )
+        internal
+    {
+        if (!isSupported[depositAsset]) {
+            revert TellerWithMultiAssetSupport__AssetNotSupported();
+        }
+
+        uint256 shareAmount = _erc20Deposit(depositAsset, depositAmount, minimumMint, msg.sender);
+        _afterPublicDeposit(msg.sender, depositAsset, depositAmount, shareAmount, shareLockPeriod);
+        bridge(shareAmount, data);
+    }
+
+    /**
+     * @notice Preview fee required to bridge shares in a given feeToken.
+     */
+    function previewFee(uint256 shareAmount, BridgeData calldata data) external view returns (uint256 fee) {
+        return _quote(shareAmount, data);
+    }
+
+    /**
+     * @notice bridging code to be done without deposit, for users who already have vault tokens
+     * @param shareAmount to bridge
+     * @param data bridge data
+     */
+    function bridge(
+        uint256 shareAmount,
+        BridgeData calldata data
+    )
+        public
+        payable
+        requiresAuth
+        returns (bytes32 messageId)
+    {
+        if (isPaused) revert TellerWithMultiAssetSupport__Paused();
+
+        _beforeBridge(data);
+
+        // Since shares are directly burned, call `beforeTransfer` to enforce before transfer hooks.
+        beforeTransfer(msg.sender);
+
+        // Burn shares from sender
+        vault.exit(address(0), ERC20(address(0)), 0, msg.sender, shareAmount);
+
+        messageId = _bridge(shareAmount, data);
+        _afterBridge(shareAmount, data, messageId);
+    }
+
+    /**
+     * @notice the virtual bridge function to be overridden
+     * @param data bridge data
+     * @return messageId
+     */
+    function _bridge(uint256 shareAmount, BridgeData calldata data) internal virtual returns (bytes32);
+
+    /**
+     * @notice the virtual function to override to get bridge fees
+     * @param shareAmount to send
+     * @param data bridge data
+     */
+    function _quote(uint256 shareAmount, BridgeData calldata data) internal view virtual returns (uint256);
+
+    /**
+     * @notice after bridge code, just an emit but can be overridden
+     * @notice the before bridge hook to perform additional checks
+     * @param data bridge data
+     */
+    function _beforeBridge(BridgeData calldata data) internal virtual;
+
+    /**
+     * @notice after bridge code, just an emit but can be overridden
+     * @param shareAmount share amount burned
+     * @param data bridge data
+     * @param messageId message id returned when bridged
+     */
+    function _afterBridge(uint256 shareAmount, BridgeData calldata data, bytes32 messageId) internal virtual {
+        emit MessageSent(messageId, shareAmount, data.destinationChainReceiver);
+    }
+
+    /**
+     * @notice a before receive hook to call some logic before a receive is processed
+     */
+    function _beforeReceive() internal virtual {
+        if (isPaused) revert TellerWithMultiAssetSupport__Paused();
+    }
+
+    /**
+     * @notice a hook to execute after receiving
+     * @param shareAmount the shareAmount that was minted
+     * @param destinationChainReceiver the receiver of the shares
+     * @param messageId the message ID
+     */
+    function _afterReceive(uint256 shareAmount, address destinationChainReceiver, bytes32 messageId) internal virtual {
+        emit MessageReceived(messageId, shareAmount, destinationChainReceiver);
+    }
+}
---
+++ src/base/Roles/TellerWithMultiAssetSupportPredicate.sol
@@ -0,0 +1,423 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { ERC20 } from "@solmate/tokens/ERC20.sol";
+import { WETH } from "@solmate/tokens/WETH.sol";
+import { BoringVault } from "src/base/BoringVault.sol";
+import { AccountantWithRateProviders } from "src/base/Roles/AccountantWithRateProviders.sol";
+import { FixedPointMathLib } from "@solmate/utils/FixedPointMathLib.sol";
+import { SafeTransferLib } from "@solmate/utils/SafeTransferLib.sol";
+import { BeforeTransferHook } from "src/interfaces/BeforeTransferHook.sol";
+import { Auth, Authority } from "@solmate/auth/Auth.sol";
+import { ReentrancyGuard } from "@solmate/utils/ReentrancyGuard.sol";
+import {PredicateClient} from "@predicate/contracts/src/mixins/PredicateClient.sol";
+import {PredicateMessage} from "@predicate/contracts/src/interfaces/IPredicateClient.sol";
+import {IPredicateManager} from "@predicate/contracts/src/interfaces/IPredicateManager.sol";
+
+
+/**
+ * @title TellerWithMultiAssetSupportPredicate
+ * @custom:security-contact security@molecularlabs.io
+ */
+contract TellerWithMultiAssetSupportPredicate is Auth, BeforeTransferHook, ReentrancyGuard, PredicateClient {
+    using FixedPointMathLib for uint256;
+    using SafeTransferLib for ERC20;
+    using SafeTransferLib for WETH;
+
+    // ========================================= CONSTANTS =========================================
+
+    /**
+     * @notice Native address used to tell the contract to handle native asset deposits.
+     */
+    address internal constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    /**
+     * @notice The maximum possible share lock period.
+     */
+    uint256 internal constant MAX_SHARE_LOCK_PERIOD = 3 days;
+
+    // ========================================= STATE =========================================
+
+    /**
+     * @notice Mapping ERC20s to an isSupported bool.
+     */
+    mapping(ERC20 => bool) public isSupported;
+
+    /**
+     * @notice The deposit nonce used to map to a deposit hash.
+     */
+    uint96 public depositNonce = 1;
+
+    /**
+     * @notice After deposits, shares are locked to the msg.sender's address
+     *         for `shareLockPeriod`.
+     * @dev During this time all transfers from msg.sender will revert, and
+     *      deposits are refundable.
+     */
+    uint64 public shareLockPeriod;
+
+    /**
+     * @notice Used to pause calls to `deposit` and `depositWithPermit`.
+     */
+    bool public isPaused;
+
+    /**
+     * @dev Maps deposit nonce to keccak256(address receiver, address depositAsset, uint256 depositAmount, uint256
+     * shareAmount, uint256 timestamp, uint256 shareLockPeriod).
+     */
+    mapping(uint256 => bytes32) public publicDepositHistory;
+
+    /**
+     * @notice Maps user address to the time their shares will be unlocked.
+     */
+    mapping(address => uint256) public shareUnlockTime;
+
+    //============================== ERRORS ===============================
+
+    error TellerWithMultiAssetSupport__ShareLockPeriodTooLong();
+    error TellerWithMultiAssetSupport__SharesAreLocked();
+    error TellerWithMultiAssetSupport__SharesAreUnLocked();
+    error TellerWithMultiAssetSupport__BadDepositHash();
+    error TellerWithMultiAssetSupport__AssetNotSupported();
+    error TellerWithMultiAssetSupport__ZeroAssets();
+    error TellerWithMultiAssetSupport__MinimumMintNotMet();
+    error TellerWithMultiAssetSupport__MinimumAssetsNotMet();
+    error TellerWithMultiAssetSupport__PermitFailedAndAllowanceTooLow();
+    error TellerWithMultiAssetSupport__ZeroShares();
+    error TellerWithMultiAssetSupport__Paused();
+    error TellerWithMultiAssetSupport__PredicateUnauthorizedTransaction();
+
+    //============================== EVENTS ===============================
+
+    event Paused();
+    event Unpaused();
+    event AssetAdded(address indexed asset);
+    event AssetRemoved(address indexed asset);
+    event Deposit(
+        uint256 indexed nonce,
+        address indexed receiver,
+        address indexed depositAsset,
+        uint256 depositAmount,
+        uint256 shareAmount,
+        uint256 depositTimestamp,
+        uint256 shareLockPeriodAtTimeOfDeposit
+    );
+    event BulkDeposit(address indexed asset, uint256 depositAmount);
+    event BulkWithdraw(address indexed asset, uint256 shareAmount);
+    event DepositRefunded(uint256 indexed nonce, bytes32 depositHash, address indexed user);
+
+    //============================== IMMUTABLES ===============================
+
+    /**
+     * @notice The BoringVault this contract is working with.
+     */
+    BoringVault public immutable vault;
+
+    /**
+     * @notice The AccountantWithRateProviders this contract is working with.
+     */
+    AccountantWithRateProviders public immutable accountant;
+
+    /**
+     * @notice One share of the BoringVault.
+     */
+    uint256 internal immutable ONE_SHARE;
+
+    constructor(
+        address _owner,
+        address _vault,
+        address _accountant,
+        address _serviceManager,
+        string memory _policyID
+    ) Auth(_owner, Authority(address(0))) {
+        vault = BoringVault(payable(_vault));
+        ONE_SHARE = 10 ** vault.decimals();
+        accountant = AccountantWithRateProviders(_accountant);
+        _initPredicateClient(_serviceManager, _policyID);
+    }
+
+    // ========================================= ADMIN FUNCTIONS =========================================
+
+    /**
+     * @notice Pause this contract, which prevents future calls to `deposit` and `depositWithPermit`.
+     * @dev Callable by MULTISIG_ROLE.
+     */
+    function pause() external requiresAuth {
+        isPaused = true;
+        emit Paused();
+    }
+
+    /**
+     * @notice Unpause this contract, which allows future calls to `deposit` and `depositWithPermit`.
+     * @dev Callable by MULTISIG_ROLE.
+     */
+    function unpause() external requiresAuth {
+        isPaused = false;
+        emit Unpaused();
+    }
+
+    /**
+     * @notice Adds this asset as a deposit asset.
+     * @dev The accountant must also support pricing this asset, else the `deposit` call will revert.
+     * @dev Callable by OWNER_ROLE.
+     */
+    function addAsset(ERC20 asset) external requiresAuth {
+        isSupported[asset] = true;
+        emit AssetAdded(address(asset));
+    }
+
+    /**
+     * @notice Removes this asset as a deposit asset.
+     * @dev Callable by OWNER_ROLE.
+     */
+    function removeAsset(ERC20 asset) external requiresAuth {
+        isSupported[asset] = false;
+        emit AssetRemoved(address(asset));
+    }
+
+    /**
+     * @notice Sets the share lock period.
+     * @dev This not only locks shares to the user address, but also serves as the pending deposit period, where
+     * deposits can be reverted.
+     * @dev If a new shorter share lock period is set, users with pending share locks could make a new deposit to
+     * receive 1 wei shares,
+     *      and have their shares unlock sooner than their original deposit allows. This state would allow for the user
+     * deposit to be refunded,
+     *      but only if they have not transferred their shares out of there wallet. This is an accepted limitation, and
+     * should be known when decreasing
+     *      the share lock period.
+     * @dev Callable by OWNER_ROLE.
+     */
+    function setShareLockPeriod(uint64 _shareLockPeriod) external requiresAuth {
+        if (_shareLockPeriod > MAX_SHARE_LOCK_PERIOD) revert TellerWithMultiAssetSupport__ShareLockPeriodTooLong();
+        shareLockPeriod = _shareLockPeriod;
+    }
+
+    // ========================================= BeforeTransferHook FUNCTIONS =========================================
+
+    /**
+     * @notice Implement beforeTransfer hook to check if shares are locked.
+     */
+    function beforeTransfer(address from) public view {
+        if (shareUnlockTime[from] > block.timestamp) revert TellerWithMultiAssetSupport__SharesAreLocked();
+    }
+
+    // ========================================= REVERT DEPOSIT FUNCTIONS =========================================
+
+    /**
+     * @notice Allows DEPOSIT_REFUNDER_ROLE to revert a pending deposit.
+     * @dev Once a deposit share lock period has passed, it can no longer be reverted.
+     * @dev It is possible the admin does not setup the BoringVault to call the transfer hook,
+     *      but this contract can still be saving share lock state. In the event this happens
+     *      deposits are still refundable if the user has not transferred their shares.
+     *      But there is no guarantee that the user has not transferred their shares.
+     * @dev Callable by STRATEGIST_MULTISIG_ROLE.
+     */
+    function refundDeposit(
+        uint256 nonce,
+        address receiver,
+        address depositAsset,
+        uint256 depositAmount,
+        uint256 shareAmount,
+        uint256 depositTimestamp,
+        uint256 shareLockUpPeriodAtTimeOfDeposit
+    )
+        external
+        requiresAuth
+    {
+        if ((block.timestamp - depositTimestamp) > shareLockUpPeriodAtTimeOfDeposit) {
+            // Shares are already unlocked, so we can not revert deposit.
+            revert TellerWithMultiAssetSupport__SharesAreUnLocked();
+        }
+        bytes32 depositHash = keccak256(
+            abi.encode(
+                receiver, depositAsset, depositAmount, shareAmount, depositTimestamp, shareLockUpPeriodAtTimeOfDeposit
+            )
+        );
+        if (publicDepositHistory[nonce] != depositHash) revert TellerWithMultiAssetSupport__BadDepositHash();
+
+        // Delete hash to prevent refund gas.
+        delete publicDepositHistory[nonce];
+
+        // Burn shares and refund assets to receiver.
+        vault.exit(receiver, ERC20(depositAsset), depositAmount, receiver, shareAmount);
+
+        emit DepositRefunded(nonce, depositHash, receiver);
+    }
+
+    // ========================================= USER FUNCTIONS =========================================
+
+
+    function deposit(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint,
+        PredicateMessage calldata predicateMessage
+    )
+        external
+        requiresAuth
+        nonReentrant
+        returns (uint256 shares)
+    {
+        bytes memory encodedSigAndArgs = abi.encodeWithSignature("_deposit(address,uint256,uint256)", depositAsset, depositAmount, minimumMint);
+        if(!_authorizeTransaction(predicateMessage, encodedSigAndArgs)){
+            revert TellerWithMultiAssetSupport__PredicateUnauthorizedTransaction();
+        }
+        return _deposit(depositAsset, depositAmount, minimumMint);
+    }
+
+    /**
+     * @notice internal helper function to deposit into BoringVault. Conforms to predicate wrapper pattern
+     */
+    function _deposit(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint
+    )
+        internal
+        returns (uint256 shares)
+    {
+        if (isPaused) revert TellerWithMultiAssetSupport__Paused();
+        if (!isSupported[depositAsset]) revert TellerWithMultiAssetSupport__AssetNotSupported();
+
+        shares = _erc20Deposit(depositAsset, depositAmount, minimumMint, msg.sender);
+
+        _afterPublicDeposit(msg.sender, depositAsset, depositAmount, shares, shareLockPeriod);
+    }
+
+    /**
+     * @notice Allows users to deposit into BoringVault using permit.
+     * @dev Publicly callable.
+     */
+    function depositWithPermit(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        external
+        requiresAuth
+        nonReentrant
+        returns (uint256 shares)
+    {
+        if (isPaused) revert TellerWithMultiAssetSupport__Paused();
+        if (!isSupported[depositAsset]) revert TellerWithMultiAssetSupport__AssetNotSupported();
+
+        // solhint-disable-next-line no-empty-blocks
+        try depositAsset.permit(msg.sender, address(vault), depositAmount, deadline, v, r, s) { }
+        catch {
+            if (depositAsset.allowance(msg.sender, address(vault)) < depositAmount) {
+                revert TellerWithMultiAssetSupport__PermitFailedAndAllowanceTooLow();
+            }
+        }
+        shares = _erc20Deposit(depositAsset, depositAmount, minimumMint, msg.sender);
+
+        _afterPublicDeposit(msg.sender, depositAsset, depositAmount, shares, shareLockPeriod);
+    }
+
+    /**
+     * @notice Allows on ramp role to deposit into this contract.
+     * @dev Does NOT support native deposits.
+     * @dev Callable by SOLVER_ROLE.
+     */
+    function bulkDeposit(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint,
+        address to
+    )
+        external
+        requiresAuth
+        nonReentrant
+        returns (uint256 shares)
+    {
+        if (!isSupported[depositAsset]) revert TellerWithMultiAssetSupport__AssetNotSupported();
+
+        shares = _erc20Deposit(depositAsset, depositAmount, minimumMint, to);
+        emit BulkDeposit(address(depositAsset), depositAmount);
+    }
+
+    /**
+     * @notice Allows off ramp role to withdraw from this contract.
+     * @dev Callable by SOLVER_ROLE.
+     */
+    function bulkWithdraw(
+        ERC20 withdrawAsset,
+        uint256 shareAmount,
+        uint256 minimumAssets,
+        address to
+    )
+        external
+        requiresAuth
+        returns (uint256 assetsOut)
+    {
+        if (!isSupported[withdrawAsset]) revert TellerWithMultiAssetSupport__AssetNotSupported();
+
+        if (shareAmount == 0) revert TellerWithMultiAssetSupport__ZeroShares();
+        assetsOut = shareAmount.mulDivDown(accountant.getRateInQuoteSafe(withdrawAsset), ONE_SHARE);
+        if (assetsOut < minimumAssets) revert TellerWithMultiAssetSupport__MinimumAssetsNotMet();
+        vault.exit(to, withdrawAsset, assetsOut, msg.sender, shareAmount);
+        emit BulkWithdraw(address(withdrawAsset), shareAmount);
+    }
+
+    /**
+    * @notice Updates the policy ID
+    * @param _policyID policy ID from onchain
+    */
+    function setPolicy(string memory _policyID) external requiresAuth {
+    policyID = _policyID;
+    serviceManager.setPolicy(_policyID);
+    }
+
+    /**
+    * @notice Function for setting the ServiceManager
+    * @param _serviceManager address of the service manager
+    */
+    function setPredicateManager(address _serviceManager) public requiresAuth {
+    serviceManager = IPredicateManager(_serviceManager);
+    }
+
+    // ========================================= INTERNAL HELPER FUNCTIONS =========================================
+
+    /**
+     * @notice Implements a common ERC20 deposit into BoringVault.
+     */
+    function _erc20Deposit(
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 minimumMint,
+        address to
+    )
+        internal
+        returns (uint256 shares)
+    {
+        if (depositAmount == 0) revert TellerWithMultiAssetSupport__ZeroAssets();
+        shares = depositAmount.mulDivDown(ONE_SHARE, accountant.getRateInQuoteSafe(depositAsset));
+        if (shares < minimumMint) revert TellerWithMultiAssetSupport__MinimumMintNotMet();
+        vault.enter(msg.sender, depositAsset, depositAmount, to, shares);
+    }
+
+    /**
+     * @notice Handle share lock logic, and event.
+     */
+    function _afterPublicDeposit(
+        address user,
+        ERC20 depositAsset,
+        uint256 depositAmount,
+        uint256 shares,
+        uint256 currentShareLockPeriod
+    )
+        internal
+    {
+        shareUnlockTime[user] = block.timestamp + currentShareLockPeriod;
+
+        uint256 nonce = depositNonce;
+        publicDepositHistory[nonce] =
+            keccak256(abi.encode(user, depositAsset, depositAmount, shares, block.timestamp, currentShareLockPeriod));
+        depositNonce++;
+        emit Deposit(nonce, user, address(depositAsset), depositAmount, shares, block.timestamp, currentShareLockPeriod);
+    }
+}
