8c07149bb1b171a56cccf05772d4f935fdeeec88
Carson <carsonpcase@gmail.com>
2025-07-07 19:50:05 -0400

    feat: tests pass for first draft of account

---
+++ src/whlp-automation/HLPAccount.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.21;
+
+import { Auth, Authority } from "@solmate/auth/Auth.sol";
+
+contract HLPAccount is Auth {
+    address public constant HLP_VAULT = 0xdfc24b077bc1425AD1DEA75bCB6f8158E10Df303;
+    uint64 public constant USDC_ID = 1;
+
+    address public immutable coreWriter;
+
+    constructor(address _owner, address _coreWriter) Auth(_owner, Authority(address(0))) {
+        coreWriter = _coreWriter;
+    }
+
+    function toPerps(uint64 amount) external requiresAuth {
+        bytes memory encodedAction = abi.encode(amount, true);
+        _sendCoreWriterCall(encodedAction, 0x07);
+    }
+
+    function toSpot(uint64 amount) external requiresAuth {
+        bytes memory encodedAction = abi.encode(amount, false);
+        _sendCoreWriterCall(encodedAction, 0x07);
+    }
+
+    function depositHLP(uint64 amount) external requiresAuth {
+        bytes memory encodedAction = abi.encode(HLP_VAULT, true, amount);
+        _sendCoreWriterCall(encodedAction, 0x02);
+    }
+
+    function withdrawHLP(uint64 amount) external requiresAuth {
+        bytes memory encodedAction = abi.encode(HLP_VAULT, false, amount);
+        _sendCoreWriterCall(encodedAction, 0x02);
+    }
+
+    function withdrawSpot(uint64 amount) external requiresAuth {
+        bytes memory encodedAction = abi.encode(owner, USDC_ID, amount);
+        _sendCoreWriterCall(encodedAction, 0x06);
+    }
+
+    // Use a raw call with no error handling since core writer will not throw errors regardless
+    function _sendCoreWriterCall(bytes memory encodedAction, bytes1 actionID) internal {
+        bytes memory data = new bytes(4 + encodedAction.length);
+        data[0] = 0x01;
+        data[1] = 0x00;
+        data[2] = 0x00;
+        data[3] = actionID;
+        for (uint256 i = 0; i < encodedAction.length; i++) {
+            data[4 + i] = encodedAction[i];
+        }
+        coreWriter.call(abi.encodeWithSignature("sendRawAction(bytes)", data));
+    }
+}
--- test/WHLPAutomation.t.sol
+++ test/WHLPAutomation.t.sol
@@ -3,9 +3,24 @@ pragma solidity 0.8.21;
 
 import { Test, stdStorage, StdStorage, stdError, console } from "@forge-std/Test.sol";
 import { MockCoreWriter } from "test/resources/MockCoreWriter.sol";
+import { HLPAccount } from "src/whlp-automation/HLPAccount.sol";
 
 contract WHLPAutomation is Test {
-    MockCoreWrtier mockCoreWriter;
+    MockCoreWriter mockCoreWriter;
+
+    event MockCoreWriter__LimitOrder(
+        uint32 asset, bool isBuy, uint64 limitPx, uint64 sz, bool reduceOnly, uint8 encodedTif, uint128 cloid
+    );
+    event MockCoreWriter__VaultTransfer(address vault, bool isDeposit, uint64 usd);
+    event MockCoreWriter__TokenDelegate(address validator, uint64 _wei, bool isUndelegate);
+    event MockCoreWriter__StakingDeposit(uint64 _wei);
+    event MockCoreWriter__StakingWithdraw(uint64 _wei);
+    event MockCoreWriter__SpotSend(address destination, uint64 token, uint64 _wei);
+    event MockCoreWriter__UsdClassTransfer(uint64 ntl, bool toPerp);
+    event MockCoreWriter__FinalizeEvmContract(
+        uint64 token, uint8 encodedFinalizeEvmContractVariant, uint64 createNonce
+    );
+    event MockCoreWriter__AddApiWallet(address apiWalletAddress, string apiWalletName);
 
     function setUp() external {
         mockCoreWriter = new MockCoreWriter();
@@ -13,40 +28,33 @@ contract WHLPAutomation is Test {
 
     // test the account in isolation
     function testHLPAccount() external {
-        HLPAccount account = new HLPAccount(address(this));
+        HLPAccount account = new HLPAccount(address(this), address(mockCoreWriter));
 
         // Assume some funds are sent on L1
 
         // transfer 100e6 USDC from spot to perps
-        vm.expectEmit(abi.encodeWithSelector(MockCoreWriter.MockCoreWriter__UsdClassTransfer.selctor, 100e6, true));
+        vm.expectEmit();
+        emit MockCoreWriter__UsdClassTransfer(100e6, true);
         account.toPerps(100e6);
 
         // deposit 100e6 USDC to HLP
-        vm.expectEmit(
-            abi.encodeWithSelector(
-                MockCoreWriter.MockCoreWriter__VaultTransfer.selctor, account.HLP_VAULT(), true, 100e6
-            )
-        );
+        vm.expectEmit();
+        emit MockCoreWriter__VaultTransfer(account.HLP_VAULT(), true, 100e6);
         account.depositHLP(100e6);
 
         // withdraw 50e6 USDC from HLP
-        vm.expectEmit(
-            abi.encodeWithSelector(
-                MockCoreWriter.MockCoreWriter__VaultTransfer.selctor, account.HLP_VAULT(), false, 50e6
-            )
-        );
+        vm.expectEmit();
+        emit MockCoreWriter__VaultTransfer(account.HLP_VAULT(), false, 50e6);
         account.withdrawHLP(50e6);
 
         // transfer 50e6 USDC to spot
-        vm.expectEmit(abi.encodeWithSelector(MockCoreWriter.MockCoreWriter__UsdClassTransfer.selctor, 50e6, false));
+        vm.expectEmit();
+        emit MockCoreWriter__UsdClassTransfer(50e6, false);
         account.toSpot(50e6);
 
         // withdraw 50e6 USDC to owner
-        vm.expectEmit(
-            abi.encodeWithSelector(
-                MockCoreWriter.MockCoreWriter__SpotSend.selctor, address(this), account.USDC_ID(), 50e6
-            )
-        );
+        vm.expectEmit();
+        emit MockCoreWriter__SpotSend(address(this), account.USDC_ID(), 50e6);
         account.withdrawSpot(50e6);
     }
 }
