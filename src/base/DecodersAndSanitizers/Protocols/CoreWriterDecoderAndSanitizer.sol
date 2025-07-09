16be8283722396badd48ca2c885da36206828830
Carson <carsonpcase@gmail.com>
2025-07-09 17:21:54 -0400

    feat: decoder for WHLP

---
+++ src/base/DecodersAndSanitizers/Protocols/CoreWriterDecoderAndSanitizer.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { BaseDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+abstract contract CoreWriterDecoderAndSanitizer is BaseDecoderAndSanitizer {
+    error CoreWriterDecoderAndSanitizer__InvalidEncodingVersion();
+    error CoreWriterDecoderAndSanitizer__InvalidActionID();
+
+    function SendRawAction(bytes calldata data) external view virtual returns (bytes memory addressesFound) {
+        if (data[0] != 0x01) {
+            revert CoreWriterDecoderAndSanitizer__InvalidEncodingVersion();
+        }
+        bytes1 actionID = data[3];
+
+        if (actionID == 0x01) {
+            // Limit Order
+            (uint32 asset, bool isBuy, uint64 limitPx, uint64 sz, bool reduceOnly, uint8 encodedTif, uint128 cloid) =
+                abi.decode(data[4:], (uint32, bool, uint64, uint64, bool, uint8, uint128));
+            limitPx / 10;
+            return abi.encodePacked(asset, isBuy);
+        } else if (actionID == 0x06) {
+            // Spot Send
+            (address destination, uint64 token, uint64 _wei) = abi.decode(data[4:], (address, uint64, uint64));
+            return abi.encodePacked(destination);
+        }
+    }
+}
