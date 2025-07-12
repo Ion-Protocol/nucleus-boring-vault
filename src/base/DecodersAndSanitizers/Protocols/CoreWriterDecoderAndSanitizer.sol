382db6516c2ec81c401a0d9354edd79407cc2a84
Carson <carsonpcase@gmail.com>
2025-07-12 14:21:58 -0400

    feat: new slippage system in core writer decoder

--- src/base/DecodersAndSanitizers/Protocols/CoreWriterDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/CoreWriterDecoderAndSanitizer.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.21;
 import { BaseDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
 
 abstract contract CoreWriterDecoderAndSanitizer is BaseDecoderAndSanitizer {
+    uint64 constant G = 10;
+
     error CoreWriterDecoderAndSanitizer__InvalidEncodingVersion();
     error CoreWriterDecoderAndSanitizer__InvalidActionID();
 
@@ -17,8 +19,7 @@ abstract contract CoreWriterDecoderAndSanitizer is BaseDecoderAndSanitizer {
             // Limit Order
             (uint32 asset, bool isBuy, uint64 limitPx, uint64 sz, bool reduceOnly, uint8 encodedTif, uint128 cloid) =
                 abi.decode(data[4:], (uint32, bool, uint64, uint64, bool, uint8, uint128));
-            limitPx / 10;
-            return abi.encodePacked(asset, isBuy);
+            return abi.encodePacked(asset, isBuy, limitPx / G, limitPx % G == 0);
         } else if (actionID == 0x06) {
             // Spot Send
             (address destination, uint64 token, uint64 _wei) = abi.decode(data[4:], (address, uint64, uint64));
