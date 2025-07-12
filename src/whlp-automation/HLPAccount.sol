9ba35ac01f535c2f6d03afe88a918600f2ecda40
Carson <carsonpcase@gmail.com>
2025-07-11 20:44:02 -0400

    feat: emergencyHatch function in account

--- src/whlp-automation/HLPAccount.sol
+++ src/whlp-automation/HLPAccount.sol
@@ -11,6 +11,7 @@ contract HLPAccount is Auth {
     address public immutable vault;
 
     error HLPAccount__FailedCall(bytes);
+    error HLPAccount__Unauthorized();
 
     constructor(address _owner, address _vault, address _coreWriter) Auth(_owner, Authority(address(0))) {
         coreWriter = _coreWriter;
@@ -40,6 +41,26 @@ contract HLPAccount is Auth {
     function withdrawSpot(uint64 amount) external requiresAuth {
         bytes memory encodedAction = abi.encode(vault, USDC_INDEX, amount);
         _sendCoreWriterCall(encodedAction, 0x06);
+    }
+
+    /**
+     * @dev Only callable by the boring vault itself
+     * For use in the event of a breaking change in core writer
+     * or other Hypercore addresses/indexes that lock user funds
+     */
+    function emergencyHatch(
+        address target,
+        bytes calldata data
+    )
+        external
+        payable
+        returns (bool success, bytes memory response)
+    {
+        if (msg.sender == vault) {
+            (success, response) = target.call{ value: msg.value }(data);
+        } else {
+            revert HLPAccount__Unauthorized();
+        }
     }
 
     // Use a raw call with no error handling since core writer will not throw errors regardless
