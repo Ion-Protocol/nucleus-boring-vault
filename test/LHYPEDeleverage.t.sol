bc7a2ed87b843ab8b3f1335270999f892d1a1c2f
Carson <carsonpcase@gmail.com>
2025-07-31 21:30:42 -0400

    test: included hyperlend

--- test/LHYPEDeleverage.t.sol
+++ test/LHYPEDeleverage.t.sol
@@ -12,26 +12,35 @@ import { RolesAuthority, Authority } from "@solmate/auth/authorities/RolesAuthor
 import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 interface IGetRate {
-    function getRate() external view returns (uint256);
+    function balancePerShare() external view returns (uint256);
 }
 
 contract LHYPEDeleverageTest is Test, MainnetAddresses {
     using stdStorage for StdStorage;
 
-    ERC20 WHYPE_DEBT = ERC20(0x37E44F3070b5455f1f5d7aaAd9Fc8590229CC5Cb);
-    ERC20 wstHYPE_COLLATERAL = ERC20(0xC8b6E0acf159E058E22c564C0C513ec21f8a1Bf5);
+    ERC20 WHYPE_DEBT_HFI = ERC20(0x37E44F3070b5455f1f5d7aaAd9Fc8590229CC5Cb);
+    ERC20 wstHYPE_COLLATERAL_HFI = ERC20(0xC8b6E0acf159E058E22c564C0C513ec21f8a1Bf5);
+
+    ERC20 WHYPE_DEBT_HLEND = ERC20(0x747d0d4Ba0a2083651513cd008deb95075683e82);
+    ERC20 wstHYPE_COLLATERAL_HLEND = ERC20(0x0Ab8AAE3335Ed4B373A33D9023b6A6585b149D33);
+
     address wstHYPE = 0x94e8396e0869c9F2200760aF0621aFd240E1CF38;
+    address stHYPE = 0xfFaa4a3D97fE9107Cef8a3F48c069F577Ff76cC1;
     address WHYPE = 0x5555555555555555555555555555555555555555;
 
-    IPool public hypurrfiPool = IPool(0xceCcE0EB9DD2Ef7996e01e25DD70e461F918A14b);
+    IPool public hypurrfiPool_hfi = IPool(0xceCcE0EB9DD2Ef7996e01e25DD70e461F918A14b);
+    IPool public hyperlendPool_hlend = IPool(0x00A89d7a5A02160f20150EbEA7a2b5E4879A1A8b);
     IUniswapV3Pool public hyperswapPool = IUniswapV3Pool(0x8D64d8273a3D50E44Cc0e6F43d927f78754EdefB);
 
     BoringVault public boringVault;
     AccountantWithRateProviders public accountant;
-    AaveV3FlashswapDeleverage public lhypeDeleverage;
+    AaveV3FlashswapDeleverage public lhypeDeleverage_hfi;
+    AaveV3FlashswapDeleverage public lhypeDeleverage_hlend;
     RolesAuthority public rolesAuthority;
 
-    IPool public pool = IPool(0xceCcE0EB9DD2Ef7996e01e25DD70e461F918A14b);
+    IPool public pool_hfi = IPool(0xceCcE0EB9DD2Ef7996e01e25DD70e461F918A14b);
+
+    uint256 wstHypeRate;
 
     function setUp() external {
         // Setup forked environment. On Hyperliquid
@@ -39,18 +48,24 @@ contract LHYPEDeleverageTest is Test, MainnetAddresses {
         uint256 blockNumber = 9_902_287;
         _startFork(rpcKey, blockNumber);
 
+        wstHypeRate = IGetRate(stHYPE).balancePerShare();
+
         boringVault = BoringVault(payable(0x5748ae796AE46A4F1348a1693de4b50560485562));
         accountant = AccountantWithRateProviders(0xcE621a3CA6F72706678cFF0572ae8d15e5F001c3);
         rolesAuthority = RolesAuthority(0xDc4605f2332Ba81CdB5A6f84cB1a6356198D11f6);
-        lhypeDeleverage = new AaveV3FlashswapDeleverage(
-            address(boringVault), address(hypurrfiPool), address(hyperswapPool), boringVault, wstHYPE, WHYPE
+        lhypeDeleverage_hfi = new AaveV3FlashswapDeleverage(
+            address(boringVault), address(hypurrfiPool_hfi), address(hyperswapPool), boringVault, wstHYPE, WHYPE
         );
-
-        vm.prank(rolesAuthority.owner());
-        rolesAuthority.setUserRole(address(lhypeDeleverage), 2, true);
+        lhypeDeleverage_hlend = new AaveV3FlashswapDeleverage(
+            address(boringVault), address(hyperlendPool_hlend), address(hyperswapPool), boringVault, wstHYPE, WHYPE
+        );
+        vm.startPrank(rolesAuthority.owner());
+        rolesAuthority.setUserRole(address(lhypeDeleverage_hfi), 2, true);
+        rolesAuthority.setUserRole(address(lhypeDeleverage_hlend), 2, true);
+        vm.stopPrank();
     }
 
-    function test_deleverage_fails_when_health_factor_below_minimum() public {
+    function test_deleverage_fails_when_health_factor_below_minimum_hfi() public {
         uint256 hypeToDeleverage = 10_000e18;
         uint256 maxStHypeWithdrawn = 10_053e18;
         uint256 minimumEndingHealthFactor = 1_190_000_000_000_000_000;
@@ -64,10 +79,10 @@ contract LHYPEDeleverageTest is Test, MainnetAddresses {
                 minimumEndingHealthFactor
             )
         );
-        lhypeDeleverage.deleverage(hypeToDeleverage, maxStHypeWithdrawn, minimumEndingHealthFactor);
+        lhypeDeleverage_hfi.deleverage(hypeToDeleverage, maxStHypeWithdrawn, minimumEndingHealthFactor);
     }
 
-    function test_deleverage_fails_when_slippage_too_high() public {
+    function test_deleverage_fails_when_slippage_too_high_hfi() public {
         uint256 hypeToDeleverage = 10_000e18;
         uint256 maxStHypeWithdrawn = 10_040e18;
         uint256 realStHypeWithdrawn = 10_052_578_589_887_917_685_505;
@@ -81,10 +96,10 @@ contract LHYPEDeleverageTest is Test, MainnetAddresses {
                 maxStHypeWithdrawn
             )
         );
-        lhypeDeleverage.deleverage(hypeToDeleverage, maxStHypeWithdrawn, minimumEndingHealthFactor);
+        lhypeDeleverage_hfi.deleverage(hypeToDeleverage, maxStHypeWithdrawn, minimumEndingHealthFactor);
     }
 
-    function test_deleverage() public {
+    function test_deleverage_hfi() public {
         // TODO: More accurate numbers here
         uint256 hypeToDeleverage = 10_000e18;
         uint256 maxStHypeWithdrawn = 10_053e18;
@@ -96,24 +111,22 @@ contract LHYPEDeleverageTest is Test, MainnetAddresses {
             uint256 liquidationThresholdBefore,
             ,
             uint256 healthFactorBefore
-        ) = pool.getUserAccountData(address(boringVault));
+        ) = pool_hfi.getUserAccountData(address(boringVault));
 
-        uint256 debtBefore = WHYPE_DEBT.balanceOf(address(boringVault));
-        uint256 collateralBefore = wstHYPE_COLLATERAL.balanceOf(address(boringVault));
+        uint256 debtBefore = WHYPE_DEBT_HFI.balanceOf(address(boringVault));
+        uint256 collateralBefore = wstHYPE_COLLATERAL_HFI.balanceOf(address(boringVault));
 
         vm.prank(address(boringVault));
-        uint256 amountWstHypePaid = lhypeDeleverage.deleverage(hypeToDeleverage, maxStHypeWithdrawn, healthFactorBefore);
+        uint256 amountWstHypePaid =
+            lhypeDeleverage_hfi.deleverage(hypeToDeleverage, maxStHypeWithdrawn, healthFactorBefore);
         console.log("liquidationThresholdBefore", liquidationThresholdBefore);
 
-        uint256 expectedHealthFactor = ((collateralBefore - amountWstHypePaid) * 1_008_857_603_665_968_334 / 1e18)
+        uint256 expectedHealthFactor = ((collateralBefore - amountWstHypePaid) * wstHypeRate / 1e18)
             * liquidationThresholdBefore * 1e18 / (debtBefore - hypeToDeleverage) / 1e4; // divide 1e4 because liquidation
             // threshold has 4 decimals
 
         (uint256 totalCollateralBaseAfter, uint256 totalDebtBaseAfter,,,, uint256 healthFactorAfter) =
-            pool.getUserAccountData(address(boringVault));
-
-        // uint256 debtAfter = WHYPE_DEBT.balanceOf(address(boringVault));
-        // uint256 collateralAfter = wstHYPE_COLLATERAL.balanceOf(address(boringVault));
+            pool_hfi.getUserAccountData(address(boringVault));
 
         console.log("collateralBefore", collateralBefore);
         // console.log("collateralAfter", collateralAfter);
@@ -128,9 +141,85 @@ contract LHYPEDeleverageTest is Test, MainnetAddresses {
 
         assertApproxEqAbs(healthFactorAfter, expectedHealthFactor, 1e13);
         assertGt(healthFactorAfter, healthFactorBefore, "Health factor should improve");
-        // assertEq(debtAfter, debtBefore - hypeToDeleverage);
-        // assertLt(ltvAfter, ltvBefore);
-        // assertLt(ltvAfter, maxLTV);
+    }
+
+    // function test_deleverage_fails_when_health_factor_below_minimum_hlend() public {
+    //     uint256 hypeToDeleverage = 10_000e18;
+    //     uint256 maxStHypeWithdrawn = 10_053e18;
+    //     uint256 minimumEndingHealthFactor = 1_190_000_000_000_000_000;
+    //     uint256 realEndingHealthFactor = 1_184_405_334_333_582_561;
+
+    //     vm.prank(address(boringVault));
+    //     vm.expectRevert(
+    //         abi.encodeWithSelector(
+    //             AaveV3FlashswapDeleverage.LHYPEDeleverage__HealthFactorBelowMinimum.selector,
+    //             realEndingHealthFactor,
+    //             minimumEndingHealthFactor
+    //         )
+    //     );
+    //     lhypeDeleverage.deleverage(hypeToDeleverage, maxStHypeWithdrawn, minimumEndingHealthFactor);
+    // }
+
+    // function test_deleverage_fails_when_slippage_too_high_hlend() public {
+    //     uint256 hypeToDeleverage = 10_000e18;
+    //     uint256 maxStHypeWithdrawn = 10_040e18;
+    //     uint256 realStHypeWithdrawn = 10_052_578_589_887_917_685_505;
+    //     uint256 minimumEndingHealthFactor = 1_170_000_000_000_000_000;
+
+    //     vm.prank(address(boringVault));
+    //     vm.expectRevert(
+    //         abi.encodeWithSelector(
+    //             AaveV3FlashswapDeleverage.LHYPEDeleverage__SlippageTooHigh.selector,
+    //             realStHypeWithdrawn,
+    //             maxStHypeWithdrawn
+    //         )
+    //     );
+    //     lhypeDeleverage.deleverage(hypeToDeleverage, maxStHypeWithdrawn, minimumEndingHealthFactor);
+    // }
+
+    function test_deleverage_hlend() public {
+        // TODO: More accurate numbers here
+        uint256 hypeToDeleverage = 10_000e18;
+        uint256 maxStHypeWithdrawn = 10_053e18;
+
+        (
+            uint256 totalCollateralBaseBefore,
+            uint256 totalDebtBaseBefore,
+            ,
+            uint256 liquidationThresholdBefore,
+            ,
+            uint256 healthFactorBefore
+        ) = hyperlendPool_hlend.getUserAccountData(address(boringVault));
+
+        uint256 debtBefore = WHYPE_DEBT_HLEND.balanceOf(address(boringVault));
+        uint256 collateralBefore = wstHYPE_COLLATERAL_HLEND.balanceOf(address(boringVault));
+
+        vm.prank(address(boringVault));
+        uint256 amountWstHypePaid =
+            lhypeDeleverage_hlend.deleverage(hypeToDeleverage, maxStHypeWithdrawn, healthFactorBefore);
+        console.log("liquidationThresholdBefore", liquidationThresholdBefore);
+
+        uint256 expectedHealthFactor = ((collateralBefore - amountWstHypePaid) * wstHypeRate / 1e18)
+            * liquidationThresholdBefore * 1e18 / (debtBefore - hypeToDeleverage) / 1e4; // divide 1e4 because liquidation
+            // threshold has 4 decimals
+
+        (uint256 totalCollateralBaseAfter, uint256 totalDebtBaseAfter,,,, uint256 healthFactorAfter) =
+            hyperlendPool_hlend.getUserAccountData(address(boringVault));
+
+        console.log("collateralBefore", collateralBefore);
+        // console.log("collateralAfter", collateralAfter);
+        console.log("debtBefore", debtBefore);
+        // console.log("debtAfter", debtAfter);
+        console.log("totalCollateralBaseBefore", totalCollateralBaseBefore);
+        console.log("totalCollateralBaseAfter", totalCollateralBaseAfter);
+        console.log("totalDebtBaseBefore", totalDebtBaseBefore);
+        console.log("totalDebtBaseAfter", totalDebtBaseAfter);
+        console.log("healthFactor before", healthFactorBefore);
+        console.log("healthFactor after", healthFactorAfter);
+        console.log("expectedHealthFactor", expectedHealthFactor);
+
+        assertApproxEqAbs(healthFactorAfter, expectedHealthFactor, 1e13);
+        assertGt(healthFactorAfter, healthFactorBefore, "Health factor should improve");
     }
 
     function _startFork(string memory rpcKey, uint256 blockNumber) internal returns (uint256 forkId) {
