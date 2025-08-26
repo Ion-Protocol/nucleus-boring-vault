48643a537a60b4c561bc6ead888672abf5f5cdd9
Carson <carsonpcase@gmail.com>
2025-08-25 20:32:56 -0400

    feat: moved getMultisig to base.s.sol so it can be re-used

--- script/deploy/DeployManagerWithTokenBalanceVerification.s.sol
+++ script/deploy/DeployManagerWithTokenBalanceVerification.s.sol
@@ -30,26 +30,4 @@ contract DeployManagerWithTokenBalanceVerification is BaseScript {
         require(ManagerWithTokenBalanceVerification(simulator).owner() == multisig, "Not owner");
         console.log("Simulator deployed. Remember to grant MANAGE role for all vaults on this chain");
     }
-
-    function getMultisig() internal returns (address) {
-        if (block.chainid == 1) {
-            return 0x0000000000417626Ef34D62C4DC189b021603f2F;
-        } else if (block.chainid == 1329) {
-            return 0xF2dE1311C5b2C1BD94de996DA13F80010453e505;
-        } else if (block.chainid == 42_161) {
-            return 0x08f6f6dD5C9B33015124e1Ea4Ea1e0B11DB342FB;
-        } else if (block.chainid == 1923) {
-            return 0xc6cC90808A3434DF28028824Fd3cefcaE4A93A88;
-        } else if (block.chainid == 98_866) {
-            return 0x823873F5E05564a2F8374c56053ac65E3Add061b;
-        } else if (block.chainid == 999) {
-            return 0x413f2e80070a069eB1051772Fdc4f0af8e8303d7;
-        } else if (block.chainid == 288) {
-            return 0x0888c3D797E13892C5e67cD802F93Ffe55Ea2826;
-        } else if (block.chainid == 1_380_012_617) {
-            return 0x6d0C5a20ac08ED00256aD224F74Ca53afF3D011d;
-        } else {
-            revert("bad chain id");
-        }
-    }
 }
