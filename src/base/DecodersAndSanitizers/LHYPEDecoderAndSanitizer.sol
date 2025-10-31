cf64cd392abdea26baa95b1ae10c9ee57abac093
CarsonCase <carson@paxoslabs.com>
2025-10-31 16:09:28 -0400

    feat: v2 circle functions

--- src/base/DecodersAndSanitizers/LHYPEDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/LHYPEDecoderAndSanitizer.sol
@@ -6,27 +6,41 @@ import {
     PendleRouterDecoderAndSanitizer,
     BaseDecoderAndSanitizer
 } from "src/base/DecodersAndSanitizers/Protocols/PendleRouterDecoderAndSanitizer.sol";
-import { UniswapV3DecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/UniswapV3DecoderAndSanitizer.sol";
+import {
+    UniswapV3DecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/UniswapV3DecoderAndSanitizer.sol";
 import { OneInchDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/OneInchDecoderAndSanitizer.sol";
 import { CurveDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/CurveDecoderAndSanitizer.sol";
-import { NativeWrapperDecoderAndSanitizer } from
-    "src/base/DecodersAndSanitizers/Protocols/NativeWrapperDecoderAndSanitizer.sol";
+import {
+    NativeWrapperDecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/NativeWrapperDecoderAndSanitizer.sol";
 import { ERC4626DecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/ERC4626DecoderAndSanitizer.sol";
 import { EigenpieDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/EigenpieDecoderAndSanitizer.sol";
 import { PirexEthDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/PirexEthDecoderAndSanitizer.sol";
-import { ThunderheadDecoderAndSanitizer } from
-    "src/base/DecodersAndSanitizers/Protocols/ThunderheadDecoderAndSanitizer.sol";
+import {
+    ThunderheadDecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/ThunderheadDecoderAndSanitizer.sol";
 import { AaveV3DecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/AaveV3DecoderAndSanitizer.sol";
-import { VelodromeV1DecoderAndSanitizer } from
-    "src/base/DecodersAndSanitizers/Protocols/VelodromeV1DecoderAndSanitizer.sol";
-import { FlashHypeDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/FlashHypeDecoderAndSanitizer.sol";
+import {
+    VelodromeV1DecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/VelodromeV1DecoderAndSanitizer.sol";
+import {
+    FlashHypeDecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/FlashHypeDecoderAndSanitizer.sol";
 import { FraxLendDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/FraxLendDecoderAndSanitizer.sol";
-import { VelodromeBuybackDecoderAndSanitizer } from
-    "src/base/DecodersAndSanitizers/Protocols/VelodromeBuybackDecoderAndSanitizer.sol";
+import {
+    VelodromeBuybackDecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/VelodromeBuybackDecoderAndSanitizer.sol";
 import { SpectraDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/SpectraDecoderAndSanitizer.sol";
 import { ValantisDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/ValantisDecoderAndSanitizer.sol";
 import { NucleusDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/NucleusDecoderAndSanitizer.sol";
-import { MorphoBlueDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/MorphoBlueDecoderAndSanitizer.sol";
+import {
+    MorphoBlueDecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/MorphoBlueDecoderAndSanitizer.sol";
+import {
+    LayerZeroOFTDecoderAndSanitizer
+} from "src/base/DecodersAndSanitizers/Protocols/LayerZeroOFTDecoderAndSanitizer.sol";
+import { CircleDecoderAndSanitizer } from "src/base/DecodersAndSanitizers/Protocols/CircleDecoderAndSanitizer.sol";
 
 contract LHYPEDecoderAndSanitizer is
     PendleRouterDecoderAndSanitizer,
@@ -46,7 +60,9 @@ contract LHYPEDecoderAndSanitizer is
     SpectraDecoderAndSanitizer,
     ValantisDecoderAndSanitizer,
     NucleusDecoderAndSanitizer,
-    MorphoBlueDecoderAndSanitizer
+    MorphoBlueDecoderAndSanitizer,
+    LayerZeroOFTDecoderAndSanitizer,
+    CircleDecoderAndSanitizer
 {
     constructor(
         address _boringVault,
